2d6b21e6e717aaaff72ba303183667d9a0f7d02f
David Revoy <info@davidrevoy.com>
2015-03-04 15:47:06 +0100

    update general credits file ( unused for the moment, but maybe for later , to display general credits )

--- credits.php
+++ credits.php
@@ -1,8 +1,9 @@
 <?php
     $translators = array(
     
-    "<b>French:</b>     David Revoy.",
-    "<b>English:</b>    David Revoy."
+    '<b>French:</b>     David Revoy.',
+    '<b>English:</b>    David Revoy.',
+    '<b>Nederlands:</b>    <a href="http://wimiso.nl">Willem Sonke</a>.'
     );
     
     sort($translators);
