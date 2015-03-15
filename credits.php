a00658f8f2db0fbac98a33d508139a8241c44f49
David Revoy <info@davidrevoy.com>
2015-03-15 20:35:34 +0100

    update credit

--- credits.php
+++ credits.php
@@ -1,6 +1,7 @@
 <?php
     $translators = array(
-    
+
+    '<b>Deutsch:</b>     <a href="https://github.com/torpak">Torpak</a>.',
     '<b>French:</b>     David Revoy.',
     '<b>English:</b>    David Revoy.',
     '<b>Nederlands:</b>    <a href="http://wimiso.nl">Willem Sonke</a>.'
