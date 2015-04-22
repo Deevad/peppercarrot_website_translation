43684032ebab226b4403b04acd51de947d7d9560
David Revoy <info@davidrevoy.com>
2015-04-22 13:48:18 +0100

    [Japanese/日本語] [full website] It's epic, my teenager heart is full of happiness ; many thanks to Guruguru !

---
+++ ja.php
@@ -0,0 +1,257 @@
+<?php
+
+$LANG = array(
+
+# Global
+'TRANSLATED_BY' 		    => '翻訳 : <a href="http://sp-cute.hatenablog.com/">guruguru</a>',
+'LANGUAGE_NAME' 			=> '日本語',
+'LANGUAGE_ISO_CODE_2_LETTER'=> 'ja',
+# http://www.w3schools.com/tags/ref_language_codes.asp --> full list
+
+################################################################################
+# Header HTML infos for search engine and title in tab:
+'PEPPERCARROT_VEGETABLE'    =>  'ペッパー&amp;キャロット', // litteral translation of spices + vegetable
+'Website_DESCRIPTION'        =>  'ペッパー&amp;キャロットのオフィシャルホームページ。魔法少女ペッパーとその猫キャロットのフリーでオープンソースなウェブコミック。魔法の薬に、魔法、そして魔法生物が出てくるファンタジー世界が舞台です。 ',
+'SUBTITLE'                  =>  'David Revoyによるオープンウェブコミック',
+
+################################################################################
+# Top menu website:
+'HOMEPAGE'      =>  'ホーム',
+'WEBCOMICS'     =>  'ウェブコミック',
+'BLOG'          =>  'ブログ',
+'PHILOSOPHY'    =>  '理念',
+'CONTRIBUTE'    =>  '協力参加',
+'SOURCES'       =>  'ソース',
+'AUTHOR'        =>  '作者',
+'FOLLOW'        =>  'ペッパー&amp;キャロットをフォロー:',
+
+################################################################################
+# Top and bottom translation panel
+'ADD_TRANSLATION'       => '翻訳追加',
+'CORRECTIONS' 			=> '修正提案',
+
+################################################################################
+# Page : Homepage
+'HOMEPAGE_BIG_TEXT'		    =>  '
+    パトロンの直接支援で運営される<br/>
+    フリーでオープンソースなウェブコミックで<br/>
+    コミック業界の常識を変える!<br/>
+',
+'HOMEPAGE_MOREINFO_BUTTON'  =>  '詳しく',
+'HOMEPAGE_LAST_EPISODE'     =>  '最新エピソード:',
+'HOMEPAGE_NEWS_UPDATE'      =>  'ニュースと更新情報:',
+'HOMEPAGE_PATREON_BUTTON'   =>  'ペッパー&amp;キャロットの<br/>パトロンになりませんか？',
+'HOMEPAGE_MOREPOSTS_BUTTON' =>  'もっと見る',
+
+################################################################################
+# Page : Webcomics
+'WEBCOMIC_EPISODE'		    =>  'ウェブコミックエピソードリスト',
+'WEBCOMIC_MAKINGOF'		    =>  'メイキング',
+'WEBCOMIC_MAKINGOF_DESCRIPTION' =>  '
+    <p> メイキングと<a href="http://www.davidrevoy.com/static4/tutorials">チュートリアル</a>は、<a href="https://www.patreon.com/davidrevoy">パトロンの&#39;ヒーロー達&#39;</a>の支援に支えられている特別ボーナスです。</p>
+',
+'WEBCOMIC_ARTWORK'		    =>  'アートワークギャラリー',
+'WEBCOMIC_SKETCHES'		    =>  'スケッチ',
+
+################################################################################
+# Page : Blog
+# when content are not English ( no need to warn user the content is english only here ) :
+#'LIMITATIONS' 	=> '<i class="fa fa-info-circle"></i> Content available in english only ',
+'LIMITATIONS' 		=> '<i class="fa fa-info-circle"></i>このセクションのコンテンツは英語のみです',
+
+################################################################################
+# Page : Philosophy
+'PHILOSOPHY_CONTENT'    =>  '
+ <h2>パトロンによる支援</h2>
+    
+    <p>ペッパー&amp;キャロットプロジェクトは、世界中からのパトロンからの資金援助だけによって支えられています。<br/>
+    パトロンは新しいエピソードが公開される度にちょっとしたお金を送金します。パトロンは各エピソードの最後にクレジットされます。<br/>
+    このシステムのおかげで、ペッパー&amp;キャロットは広告やマーケティングPRなしに、独立した運営が可能です。</p>
+    
+    <div class="philobutton">
+        <a href="https://www.patreon.com/davidrevoy" title="1エピソードたった1ドルで、ペッパー&amp;キャロットのパトロンになりませんか？">
+            1エピソードたった1ドルで、あなたもPatreonでペッパー&amp;キャロットのパトロンになりませんか？
+        </a>
+    </div>
+    
+        <img alt="パトロンシステムの説明イラスト" src="data/images/static/2015-02-09_philosophy_01-support.jpg">
+
+
+    <h2>ずっと100%無料、 課金の壁は無し</h2>
+    
+    <p>ペッパー&amp;キャロットに関連してこの私が作り出すすべてのコンテンツはこのウェブページ上に、無料ですべての人が利用できるように公開されます。<br/>
+    すべての読者に等しく敬意を払います。資金援助に参加した人もそうでない人も。パトロンのために作成したすべてのスペシャルボーナスもこのウェブサイトに投稿されます。<br/>
+    ペッパー&amp;キャロットは、新しいコンテンツのためにお金を払うことや有料サブスクリプションを強制することはしません。</p>
+    
+        <img alt="課金の壁の説明イラスト" src="data/images/static/2015-02-09_philosophy_03-paywall.jpg">
+
+    <h2>オープンソースと寛容なライセンス</h2>
+    
+    <p>私は、自分の作品を元に、みなが共有し、創作を行い、お金儲けする権利を与えたいと思っています。<br/>
+    すべてのページ、アートワーク、コンテンツはGnu/Linuxのフリーでオープンソースのソフトフェアで作成されています。ソースファイルもこのウェブサイトの&#39;ソース&#39;メニューにあります。<br/>
+    商用利用、翻訳、ファンアート作成、印刷、映像化、ゲーム化、共有、再投稿などは奨励されています。<br/>
+    その場合に必要なのは、作者として私の名前- David Revoy -をクレジットすることだけです。</p>
+    
+    <div class="philobutton">
+        <a href="https://creativecommons.org/licenses/by/3.0/" title="詳しくはCreative Commons Attributionを参照してください">
+            ライセンス : Creative Commons Attribution
+        </a>
+    </div>
+
+        <img alt="オープンソースメディアの説明イラスト" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
+
+    <h2>世界中すべての人のための高品質なエンターテイメント</h2>
+    
+    <p>ペッパー&amp;キャロットは万人、全年齢向けのコメディ/ユーモアウェブコミックです。<br/>
+    成人向けコンテンツも、暴力もありません。フリーでオープンソースで、ペッパー&amp;キャロットはフリー文化がクールであると誇らしく示します。<br/>
+    私は高品質を追求しています。フリーでオープンソースだからといって、それは下手だったりアマチュアというわけではありません。むしろその逆です。</p>
+
+        <img alt="" src="data/images/static/2015-02-09_philosophy_05-everyone.jpg">
+
+    <h2>コミック業界を変化させましょう！</h2>
+    
+    <p>読者とアーティストの距離が近ければ、読者が払うお金は少なく、作者がもらうお金は増えます。あなたは私を直接支援することになるのです。
+    自分たちの市場のビジョンに合わせてペッパー&amp;キャロットを変えようと圧力をかける出版社、流通業者、マーケティング、ファッションは、ここにはいないのです。<br/>
+    …一つでもパトロンの支援による成功例があれば、それが雪崩のように広がることだって不可能ではないでしょう。業界が危機にあるとしたら？挑戦してみましょう！</p>
+    
+        <img alt="イラスト：コミック業界vsパトロンの支援" src="data/images/static/2015-02-09_philosophy_06-industry-change.jpg">
+
+    <h2>どうしてペッパー&amp;キャロットにお金が必要なの?</h2>
+    
+    <p>私がペッパー&amp;キャロットをフルタイムで製作しつつ、屋根の下で食事ができるようにするには、1エピソードにつき$900と、1か月に2エピソードの製作が必要です。<br/>
+    この$900というのは天から降ってきた数字ではありません。フランスでの最低賃金と、独立したアーティストであるために必要な経費支払から計算した額になります。<br/>
+    $900以下だと、私はフリーランスの仕事を続ける必要があります。(ペッパー&amp;キャロットの製作ペースは遅くなり、1か月に1エピソード以下になります)<br/>
+    $900を越えれば、フルタイムでペッパー&amp;キャロットを製作できます。(ペッパー&amp;キャロットの製作ペースが速くなり、1か月に2エピソードになります) </p>
+    
+    <div class="philobutton">
+        <a href="https://www.patreon.com/davidrevoy" title="1エピソードたった1ドルで、ペッパー&amp;キャロットのパトロンになりませんか？">
+            ペッパー&amp;キャロットの製作を加速する手助けをしてください！
+        </a> 
+    </div>
+        
+    <img alt="" src="data/images/static/2015-02-09_philosophy_02-money-meter.jpg"> 
+    
+    <p>ここまで読んでいただき、ありがとうございました! <br/>
+    -David Revoy</p>
+ 
+ ',
+ 
+################################################################################
+# Page : Contribute 
+'CONTRIBUTE_TITLE'		=> '協力参加',
+'CONTRIBUTE_TOP'		=> '
+    <p><a href="?static6/sources" title="Sources page">オープンソース</a>であることと、<a href="http://creativecommons.org/licenses/by/3.0/">
+    クリエイティブコモンズライセンス</a>のおかげで、読者のあなたもいろいろな方法でペッパー&amp;キャロットに協力、参加ができます:</p>
+            
+    <h2>パトロン、寄付</h2> 
+    
+    <p><a href="?static2/philosophy" title="open the Sources page">理念のページ</a>に資金援助に関する内容のほぼすべてがあります。<br/>
+    ペッパー&amp;キャロットのパトロンになるのは簡単です。<a href="https://www.patreon.com/davidrevoy">Patreon</a>で、1エピソード$1からパトロンになることができます。<br/>
+    Patreonでは世界中のクレジットカードが使えます。PatreonではPaypalでの支払いも可能です。<br/>
+            またPayPalとFlattrでの直接の寄付も受け入れています :</p>
+',
+'CONTRIBUTE_FANART'	    => '
+    <h2>ファンアート</h2>
+    <p> ペッパー&amp;キャロットはファンアートを歓迎します。イラストも、シナリオも、フィギュアも、3Dモデルも、小説も歓迎です。ファンアートギャラリーに掲載を希望する場合は、私に作品を送ってください( <a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>へのメールか、ソーシャルネットワークサイト経由):</p>
+',
+'CONTRIBUTE_DERIVATIONS' => '
+    <h2> 派生プロジェクト :</h2>
+    <p>ペッパー&amp;キャロットを様々なプロジェクトや製品に派生させることもできます。自分でプロジェクトを開始したり、既にあるプロジェクトに参加したりしてみませんか？</p>
+',
+
+'CONTRIBUTE_BOTTOM'	    => '
+    <h2>翻訳と修正:</h2>
+    
+    <p> ペッパー&amp;キャロットウェブサイトは多言語対応のデザインになっていて、どの言語(既に使われなくなった言語でも、創作言語でも)も受け入れます。翻訳用のページのソースが用意されています。翻訳の追加方法については<a href="index.php?fr/article267/translation-tutorial">翻訳チュートリアル</a>を参照してください。</p>
+            
+    <h2>メディア、ソーシャルネットワークでのシェア</h2> 
+    
+    <p>ペッパー&amp;キャロットについて情報発信してみましょう！記事を書くこと、ウェブサイトでの紹介、ソーシャルネットワークでのシェアなどを歓迎します。「ソース」ページから<a href="?static6/sources" title="Sources page">プレスキット</a>もダウンロード可能です。</p>  
+    
+    <h2> その他の参加アイデア ...</h2>
+    
+    <p> いろいろな人がそれぞれのやり方で参加ができるはずです : <br/>
+        <b>開発者 :</b>  ペッパー&amp;キャロットを携帯端末で読めるようにするアプリケーションの開発<br />
+        <b>ミュージシャン :</b>  ペッパー&amp;キャロットの曲の作成<br />
+        <b>作家 : </b>  ペッパー&amp;キャロットのシナリオの提案<br />
+        <b>ジャーナリスト: </b> ペッパー&amp;キャロットについてメディアで情報発信(雑誌、テレビなど…)<br />
+        <b>印刷者:</b> ペッパー&amp;キャロットのポスターやグッズを印刷<br />
+    </p> 
+            
+    <h2> IRCチャンネル:</h2>
+    
+    <p> ペッパー&amp;キャロットについてチャットして話しましょう。私はヨーロッパ時間の日中に参加しています。(IRCニックネーム:deevad ) <br/> 
+',
+
+################################################################################
+# Page : Sources
+'SOURCES_TITLE'		    => 'ソース',
+'SOURCES_TOP'	        => '
+    <p><b>ソースファイルダウンロードセンターにようこそ！</b><br/>
+    ペッパー&amp;キャロットのソースファイルその他がここにあります。<br/>
+    デジタルペイントファイルのすべては、最新バージョンの<a href="https://krita.org/">Krita</a>と互換性があります。</p>
+',
+'SOURCES_BOTTOM'	    => '
+    <p>これらのファイルをダウンロードすることで、以下のライセンスに合意したものとみなします。<br/>
+    <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution license</a>
+    各プロジェクトの詳細は、READMEを参照してください。</p>
+  
+    <p><b><i class="fa fa-github" style="font-size:1.5em;" ></i> 翻訳、スクリプト、ウェブサイト、グラフィックなどのコードリポジトリ:</b><br/></p>
+',
+'SOURCE_COVER'   =>  'カバーイメージ',
+'SOURCE_KRITA'   =>  'Kritaソースファイル',
+'SOURCE_TRANSLATOR'=>  '翻訳者向けパック',
+'SOURCE_WEB'      =>  'Web向け <span class="sourceinfos"> 90ppi、軽量のjpg </span>',
+'SOURCE_PRINT'        =>  '印刷向け <span class="sourceinfos"> 300ppi、ロスレス品質 </span>',
+'SOURCE_MONTAGE'   =>  'モンタージュ <span class="sourceinfos"> すべてのパネルを1ページにまとめたもの </span>',
+
+################################################################################
+# Page : Author
+'AUTHOR_TITLE'		=> 'David Revoyについて',
+'AUTHOR_BIO'	    => '
+    <p>こんにちは、私はDavid Revoyです。1981年生まれのフランスのアーティストです。独学のアーティストで、絵を描くこと、猫、コンピュータ、Gnu/Linuxオープンソース文化、インターネット、昔ながらのRPGビデオゲーム、懐かしいマンガとアニメ、伝統的な芸術、日本文化、ファンタジーなどに夢中です…<br />
+    <br />
+    デジタルアート、講義、コンセプトアート、イラスト、アートディレクションといったフリーランスの仕事を10年以上続けてきた私は、自分自身のプロジェクトを始めることにしました。自分の好きなものすべてを詰め込んだ、その結果がペッパー&amp;キャロットです。</p>
+    
+    <p>私のポートフォリオ :</p>
+',
+'AUTHOR_TODO_DREAM'	=> '
+    <h3>夢のTODOリスト :</h3>
+
+    <p>
+    ☐ 日本のコミックイベントで、ペッパー&amp;キャロットの話をする<br/>
+    ☐ ペッパー&amp;キャロットのビデオゲームをゲームパッドで遊ぶ<br/>
+    ☐ ギャラリーに100枚のファンアートが登録される<br/>
+    ☐ Wikipediaにページができる<br/>
+    ☐ ペッパーコスプレの写真を送ってもらう<br/>
+    ☐ キャロットという名前の赤毛の猫の写真を送ってもらう<br/>
+    ☐ 500人のパトロンの支援を受ける<br/>
+    ☐ エピソード100に到達する<br/>
+    </p>
+',
+
+'AUTHOR_CARREER_TITLE'				 => '7つの吹き出しでわかる私の経歴 :',
+'AUTHOR_CARREER_BUBBLE_DESCRIPTIONS' => '<p>Blender Foundationとの長いパートナーシップ、20冊以上の本の表紙、ネットで口コミで広がりCGコミュニティから受賞したウェブコミック、2009年からのオープンソースソフトのプロフェッショナルな使用のパイオニア、5作のボードゲームと4作のビデオゲームのイラストとアートディレクション、数千コメントとシェアを受けた世界飢餓の陰陽の絵、2D CGアワードを受賞した初の100％オープンソースによる作品：不思議の国のアリス</p>',
+
+################################################################################
+# Website General : Footer
+'FOOTER_CONTENT'    => '
+    <p>ウェブコミック、アートワーク、テキストは特にページ内で追記されていない限り、CC-Byライセンスで提供されます。<br/>
+    帰属表示クレジットは "David Revoy, www.davidrevoy.com"です。 詳細情報が必要な場合の連絡先はこちらです : info@davidrevoy.com </p>
+    
+    <p>Website powered by <a href="http://www.pluxml.org" title="PlXml">PluXml</a></p> 
+',
+
+################################################################################
+# various utils :
+'UTIL_NEXT_EPISODE'      =>  '次のエピソード',
+'UTIL_PREVIOUS_EPISODE'  =>  '前のエピソード',
+'UTIL_BACK_TO_GALLERY'      =>  'ギャラリーに戻る',
+'UTIL_MORE'      =>  'もっと見る',
+'UTIL_PAGE'      =>  'ページ',
+'UTIL_BY'        =>  'by',
+'UTIL_LICENSE'   =>  'license',
+
+);
+
+?>  
