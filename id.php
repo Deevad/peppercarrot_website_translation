af37b1aaf070e55937062024474076603daa1133
David Revoy <info@davidrevoy.com>
2015-07-24 16:27:43 +0100

    [id][Indonesian] Thanks Bonaventura Aditya Perdana.

---
+++ id.php
@@ -0,0 +1,253 @@
+<?php
+
+$LANG = array(
+
+# Global
+'TRANSLATED_BY' 		    => 'Diterjemahkan oleh: <a href="https://id.wikipedia.org/wiki/Pengguna:Bonaditya">Bonaventura Aditya Perdana</a>',
+'LANGUAGE_NAME' 			=> 'Bahasa Indonesia',
+'LANGUAGE_ISO_CODE_2_LETTER'=> 'id',
+# http://www.w3schools.com/tags/ref_language_codes.asp --> full list
+
+################################################################################
+# Header HTML infos for search engine and title in tab:
+'PEPPERCARROT_VEGETABLE'    =>  'Pepper and Carrot', // litteral translation of spices + vegetable
+'Website_DESCRIPTION'        =>  'Situs resmi Pepper&amp;Carrot. Sebuah serial komik gratis dan sumber terbuka yang bercerita tentang Pepper, seorang penyihir muda yang selalu bersama kucingnya, Carrot. Mereka tinggal dalam dunia fantasi yang penuh dengan ramuan, sihir, dan pengalaman menakjubkan.',
+'SUBTITLE'                  =>  'komik sumber terbuka yang dibuat oleh David Revoy',
+
+################################################################################
+# Top menu website:
+'HOMEPAGE'      =>  'Halaman Utama',
+'WEBCOMICS'     =>  'Serial Komik',
+'BLOG'          =>  'Blog',
+'PHILOSOPHY'    =>  'Perihal',
+'CONTRIBUTE'    =>  'Berkontribusi',
+'SOURCES'       =>  'Sumber',
+'AUTHOR'        =>  'Pengarang',
+'FOLLOW'        =>  'Ikuti Pepper&amp;Carrot di:',
+
+################################################################################
+# Top and bottom translation panel
+'ADD_TRANSLATION'       => 'buat terjemahan dalam bahasa lain',
+'CORRECTIONS' 			=> 'usulkan koreksi',
+
+################################################################################
+# Page: Homepage
+'HOMEPAGE_BIG_TEXT'		    =>  '
+    Sebuah serial komik gratis dan sumber terbuka<br/>
+    yang didukung secara langsung oleh sukarelawan.<br/>
+    Komik ini juga hidup karena donasi dari Anda!<br/>
+',
+'HOMEPAGE_PATREON_INFO'     =>  'Hanya 1$ untuk setiap episode baru, jadilah donator melalui Patreon',
+'HOMEPAGE_MOREINFO_BUTTON'  =>  'Informasi selengkapnya',
+'HOMEPAGE_PATREON_BUTTON'   =>  'Jadilah donator dari Pepper&amp;Carrot di Patreon',
+'HOMEPAGE_LAST_EPISODE'     =>  'Episode terbaru:',
+'HOMEPAGE_NEWS_UPDATE'      =>  'Berita terbaru:',
+'HOMEPAGE_MOREPOSTS_BUTTON' =>  'Post lainnya',
+'HOMEPAGE_MAINSERVICE_LINK' =>  'https://www.patreon.com/davidrevoy',
+
+################################################################################
+# Page: Webcomics
+'WEBCOMIC_EPISODE'		    =>  'Serial komik',
+'WEBCOMIC_MAKINGOF'		    =>  'Cara membuat',
+'WEBCOMIC_MAKINGOF_DESCRIPTION' =>  '
+    <p> Bagaimana cara membuat dan <a href="http://www.davidrevoy.com/static4/tutorials">tutorial</a> merupakan bonus spesial untuk <a href="https://www.patreon.com/davidrevoy">donator di Patreon</a></p>
+',
+'WEBCOMIC_ARTWORK'		    =>  'Galeri',
+'WEBCOMIC_SKETCHES'		    =>  'Sketsa',
+
+################################################################################
+# Page: Blog
+# when content are not English (no need to warn user the content is english only here):
+#'LIMITATIONS' 	=> '<i class="fa fa-info-circle"></i> Content available in english only ',
+'LIMITATIONS' 		=> '<i class="fa fa-info-circle"></i> Konten hanya tersedia dalam bahasa Inggris ',
+
+################################################################################
+# Page: Philosophy
+'PHILOSOPHY_CONTENT'    =>  '
+ <h2>Didukung oleh sukarelawan!</h2>
+    
+    <p>Pepper&amp;Carrot mendapatkan dana dari para sukarelawan (<i>patrons</i>), yang berasal dari seluruh dunia.<br/>
+    Setiap sukarelawan dapat berdonasi untuk setiap episode baru yang akan dibuat dan nama mereka akan dicantumkan pada akhir halaman komik.<br/>
+    Pepper&amp;Carrot tidak menggunakan iklan maupun media yang serupa untuk tetap hidup.</p>
+    
+    <div class="philobutton">
+        <a href="https://www.patreon.com/davidrevoy" title="Hanya 1$ untuk setiap episode baru, jadilah donator melalui Patreon">
+            Hanya 1$ untuk setiap episode baru, jadilah donator melalui Patreon
+        </a>
+    </div>
+    
+        <a href="https://www.patreon.com/davidrevoy" title="Hanya 1$ untuk setiap episode baru, bantulah Pepper&amp;Carrot melalui Patreon">
+            <img alt="illustration representing patronage" src="data/images/static/2015-02-09_philosophy_01-support.jpg">
+        </a>
+
+    <h2>100% gratis, selamanya! Sungguh!</h2>
+    
+    <p>Setiap konten Pepper&amp;Carrot yang saya buat di situs ini, semuanya gratis dan terbuka untuk semua orang.<br/>
+    Saya akan bersikap adil: bagi yang melakukan donasi maupun tidak. Bonus spesial akan saya berikan untuk siapa saja.<br/>
+    Pepper&amp;Carrot tidak akan memaksa Anda untuk membayar atau berlangganan agar mendapatkan konten terbaru.</p>
+    
+        <img alt="illustration representing paywall" src="data/images/static/2015-02-09_philosophy_03-paywall.jpg">
+
+    <h2>Sumber terbuka dan bebas</h2>
+    
+    <p>Saya ingin memberikan kepada orang hak untuk menyebarluaskan, menggunakan, membuat dan menghasilkan keuntungan dari karya yang saya buat.<br/>
+    Semua halaman, karya dan konten dari situs ini dibuat menggunakan program yang bebas dan gratis melalui Gnu/Linux, dan semua sumber dari situs ini tersedia di menu &#39;Sumber&#39;.<br/>
+    Penggunaan komersial, penerjemahan, karya dari penggemar, mencetak, membuat film, permainan video, berbagi, dan menyebarluaskan akan sangat dihargai.<br/>
+    Anda tinggal menyebutkan saja nama saya - David Revoy - sebagai pemilik karya ini.</p>
+    
+    <div class="philobutton">
+        <a href="https://creativecommons.org/licenses/by/4.0/" title="Untuk informasi selengkapnya, silakan membaca Atribusi Creative Commons 4.0">
+            Lisensi: Atribusi Creative Commons 4.0
+        </a>
+    </div>
+
+        <img alt="illustration representing open-source medias" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
+
+    <h2>Hiburan berkualitas untuk siapa saja dan di mana saja</h2>
+    
+    <p>Pepper&amp;Carrot adalah serial komik humor yang sesuai dengan berbagai rentang usia maupun demografi di seluruh dunia.<br/>
+    Tidak ada konten dewasa, maupun kekerasan. Pepper&amp;Carrot merupakan contoh yang baik, di mana saat ini banyak kultur kebebasan yang bersifat negatif.<br/>
+    Saya berfokus pada kualitas, karena bukan berarti sesuatu yang bebas dan gratis selalu tidak bagus. Benar bukan?</p>
+
+        <img alt="" src="data/images/static/2015-02-09_philosophy_05-everyone.jpg">
+
+    <h2>Saatnya melakukan revolusi dalam dunia percetakan!</h2>
+    
+    <p>Dengan tidak adanya jarak antara pembaca dengan seniman/pengarang, Anda membayar sedikit dan saya akan mendapatkan keuntungan. Intinya, Anda mendukung saya secara langsung.<br/>
+    Tidak akan ada pihak percetakan/distributor/penjualan yang akan memaksa saya untuk mengubah Pepper&amp;Carrot sesuai &#39;keinginan mereka atau pasar&#39;.<br/>
+    &nbsp;.&nbsp;.&nbsp;.&nbsp;Perubahan kecil dapat membuat hal yang besar. Mari kita coba!</p>
+    
+        <img alt="illustration: comic book industry VS patreon support" src="data/images/static/2015-02-09_philosophy_06-industry-change.jpg">
+    
+    <div class="philobutton">
+        <a class="h4" href="https://www.patreon.com/davidrevoy" title="Hanya 1$ untuk setiap episode baru, jadilah donator melalui Patreon">
+            Bantu saya untuk terus melanjutkan Pepper&amp;Carrot!
+        </a> 
+    </div>
+            
+    <p>Terima kasih sudah membaca! <br/>
+    -David Revoy</p>
+ 
+ ',
+ 
+################################################################################
+# Page: Contribute 
+'CONTRIBUTE_TITLE'		=> 'Berkontribusi',
+'CONTRIBUTE_TOP'		=> '
+    <p>Terima kasih kepada <a href="?static6/sources" title="Sumber halaman">sumber terbuka</a> dan <a href="http://creativecommons.org/licenses/by/4.0/">
+    Lisensi Creative Commons</a>, Anda dapat berkontribusi untuk Pepper&amp;Carrot dengan berbagai macam cara:</p>
+',
+'CONTRIBUTE_DONATION'		=> '
+    <h2>Donasi melalui Patron</h2> 
+    
+    <p>Saya menjelaskan secara detail di <a href="?static2/philosophy" title="buka halaman Perihal">halaman Perihal</a> mengenai segalanya tentang berdonasi.<br/>
+    Sangat mudah untuk menjadi donator Pepper&amp;Carrot, hanya 1$ untuk setiap episode baru <a href="https://www.patreon.com/davidrevoy">melalui Patreon</a>.<br/>
+    Patreon menerima kartu kredit dari seluruh dunia, dan Anda juga dapat membayar dengan akun PayPal di Patreon.<br/>
+            I juga menerima donasi secara langsung melalui PayPal atau Flattr:</p>
+',
+'CONTRIBUTE_FANART'	    => '
+    <h2>Karya dari penggemar</h2>
+    <p> Pepper&amp;Carrot terbuka untuk karya penggemar: gambar, skenario, patung, model 3D, dan lainnya. Kirimkan semuanya ke <a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a> atau hubungi saya melalui media sosial untuk menampilkan karya dari penggemar buatan Anda:</p>
+',
+'CONTRIBUTE_DERIVATIONS' => '
+    <h2>Karya lain:</h2>
+    <p>Pepper&amp;Carrot dapat diadaptasikan untuk banyak proyek maupun produk, apakah Anda tertarik untuk membuatnya?</p>
+',
+
+'CONTRIBUTE_BOTTOM'	    => '
+    <h2>Penerjemahan dan koreksi:</h2>
+    
+    <p>Situs Pepper&amp;Carrot dibuat untuk mendukung semua bahasa (termasuk bahasa yang sudah langka, atau fiktif). Kunjungi <a href="index.php?fr/article267/translation-tutorial">halaman tutorial</a> untuk informasi lengkap mengenai bagaimana cara menambahkan terjemahan maupun melakukan koreksi.</p>
+            
+    <h2>Pers, media sosial</h2> 
+    
+    <p>Jadilah orang yang membantu Pepper&amp;Carrot! Menulis artikel, membuat situs, bangun sebuah komunitas di media sosial. Anda dapat mengunduh <a href="?static6/sources" title="halaman Sumber">peralatan pers</a> di halaman "sumber".</p>  
+    
+    <h2> Ide lain&nbsp;.&nbsp;.&nbsp;.&nbsp;</h2>
+    
+    <p> Semua orang dapat berkontribusi dengan cara lain: <br/>
+        <b>Pengembang aplikasi:</b>  membuat aplikasi untuk membaca Pepper&amp;Carrot di ponsel.<br />
+        <b>Musisi:</b>  membuat musik tema untuk Pepper&amp;Carrot.<br />
+        <b>Penulis: </b>  buat skenario cerita untuk Pepper&amp;Carrot.<br />
+        <b>Wartawan: </b> memberitakan Pepper&amp;Carrot di media (cetak, televisi, internet, dsb.)<br />
+        <b>Cetak:</b> cetak poster atau cendera mata Pepper&amp;Carrot.<br />
+    </p> 
+            
+    <h2> IRC:</h2>
+    
+    <p> Diskusikan tentang Pepper&amp;Carrot. Saya akan ada di sana saat hari kerja waktu Eropa (nama pengguna: deevad) <br/> 
+',
+
+################################################################################
+# Page: Sources
+'SOURCES_TITLE'		    => 'Sumber',
+'SOURCES_TOP'	        => '
+    <p><b>Selamat datang!</b><br/>
+    Anda akan menemukan sumber berkas dari Pepper&amp;Carrot dan yang lainnya.<br/>
+    Berkas gambar digital cocok dengan versi terakhir dari <a href="https://krita.org/">Krita</a>.</p>
+',
+'SOURCES_BOTTOM'	    => '
+    <p>Dengan mengunduh dan menggunakan berkas ini, Anda setuju dengan ketentuan <br/>
+    <a href="http://creativecommons.org/licenses/by/4.0/">lisensi atribusi Creative Commons</a>.
+    Kunjungi README dari setiap proyek untuk info selengkapnya.</p>
+  
+    <p><b><i class="fa fa-github" style="font-size:1.5em;" ></i> Gudang kode untuk terjemahan, skrip, situs, gambar, dan yang lainnya:</b><br/></p>
+',
+'SOURCE_COVER'   =>  'Halaman sampul',
+'SOURCE_KRITA'   =>  'Sumber gambar Krita',
+'SOURCE_TRANSLATOR'=>  'Untuk penerjemahan',
+'SOURCE_WEB'      =>  'Situs web <span class="sourceinfos"> 90ppi, berkas .jpg ukuran kecil </span>',
+'SOURCE_PRINT'        =>  'Cetak <span class="sourceinfos"> 300ppi, kualitas rendah </span>',
+'SOURCE_MONTAGE'   =>  'Montase <span class="sourceinfos"> Semua panel dalam satu halaman </span>',
+
+################################################################################
+# Page: Author
+'AUTHOR_TITLE'		=> 'Tentang David Revoy',
+'AUTHOR_BIO'	    => '
+    <p>Halo, nama saya David Revoy dan saya merupakan seniman Perancis yang lahir pada tahun 1981. Saya memiliki kegemaran tentang gambar, melukis, kucing, komputer, Gnu/Linux, Internet, permainan video RPG lama, manga dan anime, seni tradisional, budaya Jepang, fantasi&nbsp;.&nbsp;.&nbsp;.&nbsp;<br />
+    <br />
+    Setelah selama 10 years menjadi freelancer di gambar digital, mengajar, seni konsep, ilustrasi, arah seni, saya memutuskan untuk memulai proyek saya sendiri. Saya akhirnya menemukan semuanya itu; hasilnya adalah Pepper&amp;Carrot.</p>
+    
+    <p>Portfolio:</p>
+',
+'AUTHOR_TODO_DREAM'	=> '
+    <h3>Keinginan:</h3>
+
+    <p>
+    ☐ Menjadi pembicara dalam konvensi komik Jepang tentang Pepper&amp;Carrot<br/>
+    ☐ Bermain permainan Pepper&amp;Carrot<br/>
+    ☐ Mendapatkan 100 gambar karya penggemar<br/>
+    ☐ Memiliki halaman Wikipedia<br/>
+    ☐ Mendapatkan foto cosplay Pepper<br/>
+    ☐ Mendapatkan foto kucing merah yang bernama Carrot<br/>
+    ☐ Didukung oleh 500 donator di Patreon<br/>
+    ☐ Mencapai episode 100<br/>
+    </p>
+',
+
+'AUTHOR_CARREER_TITLE'				 => 'yang saya kerjakan:',
+'AUTHOR_CARREER_BUBBLE_DESCRIPTIONS' => '<p></p>',
+
+################################################################################
+# Website General: Footer
+'FOOTER_CONTENT'    => '
+    <p>Serial komik, gambar dan teks dilisensi di bawah CC-BY, terkecuali ada informasi lainnya di halaman tersebut.<br/>
+    Dibuat oleh "David Revoy, www.davidrevoy.com". Hubungi saya: info@davidrevoy.com untuk informasi selengkapnya.</p>
+    
+    <p>Situs dibuat oleh <a href="http://www.pluxml.org" title="PlXml">PluXml</a></p> 
+',
+
+################################################################################
+# various utils:
+'UTIL_NEXT_EPISODE'      =>  'episode selanjutnya',
+'UTIL_PREVIOUS_EPISODE'  =>  'episode sebelumnya',
+'UTIL_EPISODE'   =>  'episode',
+'UTIL_BACK_TO_GALLERY'      =>  'kembali ke galeri',
+'UTIL_MORE'      =>  'selengkapnya',
+'UTIL_PAGE'      =>  'halaman',
+'UTIL_BY'        =>  'oleh',
+'UTIL_LICENSE'   =>  'lisensi',
+
+);
+
+?>  
