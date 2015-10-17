dbf98a1064e89e93a98a88443080dd62759fef5e
Remi Rampin <remirampin@gmail.com>
2015-10-16 22:08:53 -0400

    Update en.php

--- en.php
+++ en.php
@@ -11,7 +11,7 @@ $LANG = array(
 ################################################################################
 # Header HTML infos for search engine and title in tab:
 'PEPPERCARROT_VEGETABLE'    =>  'Pepper and Carrot', // litteral translation of spices + vegetable
-'Website_DESCRIPTION'        =>  'Official homepage of Pepper&amp;Carrot, a free/libre and open-source webcomic about Pepper, a young witch and her cat, Carrot. They live in a fantasy universe of potions, magics, and creatures.',
+'Website_DESCRIPTION'        =>  'Official homepage of Pepper&amp;Carrot, a free/libre and open-source webcomic about Pepper, a young witch and her cat, Carrot. They live in a fantasy universe of potions, magic, and creatures.',
 'SUBTITLE'                  =>  'the Open Webcomic by David Revoy',
 
 ################################################################################
@@ -50,7 +50,7 @@ $LANG = array(
 'WEBCOMIC_EPISODE'		    =>  'Webcomic episodes',
 'WEBCOMIC_MAKINGOF'		    =>  'Making-of',
 'WEBCOMIC_MAKINGOF_DESCRIPTION' =>  '
-    <p> Making-of and <a href="http://www.davidrevoy.com/static4/tutorials"> art tutorials</a>  are special bonus sponsored by my <a href="https://www.patreon.com/davidrevoy">Patreons &#39;heroes&#39;</a></p>
+    <p> Making-of and <a href="http://www.davidrevoy.com/static4/tutorials">art tutorials</a>  are special bonuses sponsored by my <a href="https://www.patreon.com/davidrevoy">Patreons &#39;heroes&#39;</a></p>
 ',
 'WEBCOMIC_ARTWORK'		    =>  'Artwork gallery',
 'WEBCOMIC_SKETCHES'		    =>  'Sketches',
@@ -67,32 +67,32 @@ $LANG = array(
  <h2>Supported by patrons</h2>
 
     <p>Pepper&amp;Carrot project is only funded by its patrons, from all around the world.<br/>
-    Each patrons send a bit of money for each new episode published and get a credit at the end of the new episode.<br/>
-    Thanks to this system, Pepper&amp;Carrot can be independent and never use advertising or any marketing pollution.</p>
+    Each patron sends a little money for each new episode published and gets a credit at the end of the new episode.<br/>
+    Thanks to this system, Pepper&amp;Carrot can stay independent and never have to resort to advertising or any marketing pollution.</p>
 
     <div class="philobutton">
-        <a href="https://www.patreon.com/davidrevoy" title="For only 1$ per new episode, become a patreon of Pepper&amp;Carrot">
-            For only 1$ per new episode, become a patron today on Patreon
+        <a href="https://www.patreon.com/davidrevoy" title="For only $1 per new episode, become a patreon of Pepper&amp;Carrot">
+            For only $1 per new episode, become a patron today on Patreon
         </a>
     </div>
 
-        <a href="https://www.patreon.com/davidrevoy" title="For only 1$ per new episode, become a patreon of Pepper&amp;Carrot">
+        <a href="https://www.patreon.com/davidrevoy" title="For only $1 per new episode, become a patreon of Pepper&amp;Carrot">
             <img alt="illustration representing patronage" src="data/images/static/2015-02-09_philosophy_01-support.jpg">
         </a>
 
     <h2>100% free, forever, no paywall</h2>
 
-    <p>Every contents I produce about Pepper&amp;Carrot are on this website, free and available for everyone.<br/>
-    I respect all of you equally: with or without money. All special bonus I do for my patrons are also posted here.<br/>
-    Pepper&amp;Carrot will never ask you to pay something or get a subscription to get access to a new content.</p>
+    <p>All the content I produce about Pepper&amp;Carrot are on this website, free and available to everyone.<br/>
+    I respect all of you equally: with or without money. All special bonuses I make for my patrons are also posted here.<br/>
+    Pepper&amp;Carrot will never ask you to pay anything or to get a subscription to get access to new content.</p>
 
         <img alt="illustration representing paywall" src="data/images/static/2015-02-09_philosophy_03-paywall.jpg">
 
     <h2>Open-source and permissive</h2>
 
     <p>I want to give people the right to share, use, build and even make money upon the work I&#39;ve created.<br/>
-    All pages, artworks and content were made with Free/Libre Open-Sources Software on Gnu/Linux, and all sources are on this website, &#39;Source&#39; menu.<br/>
-    Commercial usage, translations, fan-arts, printing, movies, video-games, sharing, repost are encouraged.<br/>
+    All pages, artworks and content were made with Free/Libre Open-Sources Software on GNU/Linux, and all sources are on this website, &#39;Source&#39; menu.<br/>
+    Commercial usage, translations, fan-arts, prints, movies, video-games, sharing, and reposts are encouraged.<br/>
     You just need to credit my name - David Revoy - as the author.</p>
 
     <div class="philobutton">
@@ -101,27 +101,27 @@ $LANG = array(
         </a>
     </div>
 
-        <img alt="illustration representing open-source medias" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
+        <img alt="illustration representing open-source media" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
 
     <h2>Quality entertainment for everyone, everywhere</h2>
 
     <p>Pepper&amp;Carrot is a comedy/humor webcomic suited for everyone, every age.<br/>
-    No mature content, no violence. Free and open-source, Pepper&amp;Carrot is a proud example of how cool could be free-culture.<br/>
+    No mature content, no violence. Free and open-source, Pepper&amp;Carrot is a proud example of how cool free-culture could be.<br/>
     I&#39;m focusing a lot on quality, because free and open-source doesn&#39;t mean bad or amateur. Au contraire.</p>
 
         <img alt="" src="data/images/static/2015-02-09_philosophy_05-everyone.jpg">
 
     <h2>Let&#39;s change comic industry!</h2>
 
-    <p>With less intermediary between audience and artist you pay less and I benefit more. You support me directly.<br/>
+    <p>Without intermediary between artist and audience you pay less and I benefit more. You support me directly.<br/>
     No publisher/distributor/marketing/fashion can force me to change Pepper&amp;Carrot to fit their vision of &#39;the market&#39;.<br/>
-    &nbsp;.&nbsp;.&nbsp;.&nbsp;Why not a single success couldn&#39;t &#39;snowball&#39; a whole industry in crisis? Let&#39;s try it!</p>
+    &nbsp;.&nbsp;.&nbsp;.&nbsp;Why not a single success couldn&#39;t &#39;snowball&#39; a whole industry in crisis? We&#39;ll see...</p>
 
         <img alt="illustration: comic book industry VS patreon support" src="data/images/static/2015-02-09_philosophy_06-industry-change.jpg">
 
     <div class="philobutton">
-        <a class="h4" href="https://www.patreon.com/davidrevoy" title="For only 1$ per new episode, become a patreon of Pepper&amp;Carrot">
-            help me to boost Pepper&amp;Carrot production today
+        <a class="h4" href="https://www.patreon.com/davidrevoy" title="For only $1 per new episode, become a patreon of Pepper&amp;Carrot">
+            help me boost Pepper&amp;Carrot production today
         </a>
     </div>
 
@@ -141,41 +141,41 @@ $LANG = array(
     <h2>Patronage, donations</h2>
 
     <p>I detail in the <a href="?static2/philosophy" title="open the Sources page">Philosophy page</a> almost everything concerning funding.<br/>
-    It&#39;s easy to become the patron of Pepper&amp;Carrot for only 1$ per new episode <a href="https://www.patreon.com/davidrevoy">on Patreon</a>.<br/>
+    It&#39;s easy to become the patron of Pepper&amp;Carrot for only $1 per new episode <a href="https://www.patreon.com/davidrevoy">on Patreon</a>.<br/>
     Patreon accept credit cards from all around the world, and you can even pay with a Paypal account on Patreon.<br/>
-            I also accept direct donation via PayPal or Flattr:</p>
+            I also accept direct donations via PayPal or Flattr:</p>
 ',
 'CONTRIBUTE_FANART'	    => '
     <h2>Fan-art</h2>
-    <p> Pepper&amp;Carrot is open to fan-arts: Drawing, scenarios, scupltures, 3D models, fan-fiction. Send them to me (<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a> , or poke me on social networks) to appear on the fan-art gallery:</p>
+    <p>Pepper&amp;Carrot is open to fan-arts: Drawing, scenarios, scupltures, 3D models, fan-fiction. Send them to me (<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>, or poke me on social networks) to appear in the fan-art gallery:</p>
 ',
 'CONTRIBUTE_DERIVATIONS' => '
-    <h2> Derivations:</h2>
+    <h2>Derivatives:</h2>
     <p>Pepper&amp;Carrot can be adapted to many project or products, why not do your own or join an existing one?</p>
 ',
 
 'CONTRIBUTE_BOTTOM'	    => '
     <h2>Translations and corrections:</h2>
 
-    <p> Pepper&amp;Carrot website is designed to be multilingual and accept any language (including extinct ones, or fictive ones). The sources of this page are at your disposal to translate them. Check the <a href="index.php?fr/article267/translation-tutorial">reference tutorial</a> for more informations on how to add your translation.</p>
+    <p>Pepper&amp;Carrot website is designed to be multilingual and accept any language (including extinct ones, or fictive ones). The sources of this page are at your disposal for you to translate them. Check the <a href="index.php?fr/article267/translation-tutorial">reference tutorial</a> for more information on how to add your translation.</p>
 
     <h2>Press, Social networks</h2>
 
-    <p>Be the publisher of Pepper&amp;Carrot! Write articles, create post on website, share and build a community on your favorite social networks. You can download the <a href="?static6/sources" title="Sources page">press kit</a> on the "source" page.</p>
+    <p>Be the publisher of Pepper&amp;Carrot! Write articles, create posts on websites, share and build a community on your favorite social networks. You can download the <a href="?static6/sources" title="Sources page">press kit</a> on the "sources" page.</p>
 
-    <h2> Other contributions ideas&nbsp;.&nbsp;.&nbsp;.&nbsp;</h2>
+    <h2>Other contributions ideas&nbsp;.&nbsp;.&nbsp;.&nbsp;</h2>
 
-    <p> Everyone can contribute in different ways: <br/>
-        <b>Developers:</b>  Creation of an application to read Pepper&amp;Carrot on mobile device.<br />
-        <b>Musician:</b>  create music themes for Pepper&amp;Carrot.<br />
-        <b>Writer: </b>  propose new Pepper&amp;Carrot scenario.<br />
-        <b>Journalist: </b> Inform about Pepper&amp;Carrot in traditionnal media (printed press, tv, etc.)<br />
-        <b>Printer:</b> Print posters or goodies with Pepper&amp;Carrot on it.<br />
+    <p>Everyone can contribute in different ways:<br/>
+        <b>Developers:</b> create of an application to read Pepper&amp;Carrot on mobile device.<br/>
+        <b>Musician:</b> create music themes for Pepper&amp;Carrot.<br/>
+        <b>Writer:</b> propose new Pepper&amp;Carrot scenario.<br/>
+        <b>Journalist:</b> Inform about Pepper&amp;Carrot in traditionnal media (printed press, tv, etc.)<br/>
+        <b>Printer:</b> Print posters or goodies with Pepper&amp;Carrot on it.<br/>
     </p>
 
-    <h2> IRC channel:</h2>
+    <h2>IRC channel:</h2>
 
-    <p> Chat and discuss about Pepper&amp;Carrot. I&#39;ll be around at european day time (nickname:deevad) <br/>
+    <p>Chat and discuss about Pepper&amp;Carrot. I&#39;ll be around at european day time (nickname:deevad)<br/>
 ',
 
 ################################################################################
@@ -204,8 +204,8 @@ $LANG = array(
 # Page: Author
 'AUTHOR_TITLE'		=> 'About David Revoy',
 'AUTHOR_BIO'	    => '
-    <p>Hi, my name is David Revoy and I&#39;m a french artist born in 1981. I&#39;m self-taught and passionate about drawing, painting, cats, computers, Gnu/Linux open-source culture, Internet, old school RPG video-games, old mangas and anime, traditionnal art, japanese culture, fantasy&nbsp;.&nbsp;.&nbsp;.&nbsp;<br />
-    <br />
+    <p>Hi, my name is David Revoy and I&#39;m a french artist born in 1981. I&#39;m self-taught and passionate about drawing, painting, cats, computers, Gnu/Linux open-source culture, Internet, old school RPG video-games, old mangas and anime, traditionnal art, japanese culture, fantasy&nbsp;.&nbsp;.&nbsp;.&nbsp;<br/>
+    <br/>
     After more than 10 years of Freelance in digital-painting, teaching, concept-art, illustration, art-direction, I decided to start my own project. I finally found a way to mix all my passions together; and the result is Pepper&amp;Carrot.</p>
 
     <p>my portfolio:</p>
