4f36ee8173ffea7ce17f4bb4b902aba158511519
David Revoy <info@davidrevoy.com>
2016-09-12 21:32:43 +0100

    sz/Ślōnskŏ gŏdka lang by Grzegorz Kulik, thanks!

---
+++ sz.php
@@ -0,0 +1,259 @@
+<?php
+
+$LANG = array(
+
+# Global
+'TRANSLATED_BY' 		    => 'przekłŏd: Grzegorz Kulik',
+'LANGUAGE_NAME' 			=> 'Ślōnskŏ gŏdka',
+'LANGUAGE_ISO_CODE_2_LETTER'=> 'sz',
+# http://www.w3schools.com/tags/ref_language_codes.asp --> full list
+
+################################################################################
+# Header HTML infos for search engine and title in tab:
+'PEPPERCARROT_VEGETABLE'    =>  'Korzynie i Markewka', // litteral translation of spices + vegetable
+'Website_DESCRIPTION'        =>  'Ôficjalnŏ zajta Pepper&amp;Carrot, darmego i ze ôdewrzōnym źrōdłym necowego kōmiksu o Pepper, modyj heksie i Carrot, ôd nij kocie. Żyjōm we fantastycznym uniwersōm połnym mikstur, magije i stworōw.',
+'SUBTITLE'                  =>  'ôdewrzōny internetowy kōmiks wytworzōny ôd Davida Revoya',
+
+################################################################################
+# Top menu website:
+'HOMEPAGE'      =>  'Przodniŏ strōna',
+'WEBCOMICS'     =>  'Kōmiks',
+'BLOG'          =>  'Blog',
+'PHILOSOPHY'    =>  'Filozofijŏ',
+'CONTRIBUTE'    =>  'Spōmōż nŏs',
+'SOURCES'       =>  'Źrōdła',
+'AUTHOR'        =>  'Autōr',
+'FOLLOW'        =>  'Filuj za Pepper&amp;Carrot na:',
+
+################################################################################
+# Top and bottom translation panel
+'ADD_TRANSLATION'       => 'przidej przekłŏd',
+'CORRECTIONS' 			=> 'doradź poprŏwki',
+
+################################################################################
+# Page: Homepage
+'HOMEPAGE_BIG_TEXT'		    =>  '
+    Darmy i ôdewrzōny internetowy kōmiks<br/>
+    spiyrany bezpostrzeniŏ bez czytŏczōw,<br/>
+    coby zmiynić branżã kōmiksōw!<br/>
+',
+'HOMEPAGE_PATREON_INFO'     =>  'Wystyknie 1$ na Patreōnie, coby być patrōnym nowyj tajle',
+'HOMEPAGE_MOREINFO_BUTTON'  =>  'Wiyncyj informacyji',
+'HOMEPAGE_PATREON_BUTTON'   =>  'wesprzij nowõ tajlã Pepper&amp;Carrot na Patreōnie',
+'HOMEPAGE_LAST_EPISODE'     =>  'Ôstatniŏ tajla:',
+'HOMEPAGE_NEWS_UPDATE'      =>  'Nowiny i uwiadōmiynia:',
+'HOMEPAGE_MOREPOSTS_BUTTON' =>  'Wiyncyj postōw',
+'HOMEPAGE_MAINSERVICE_LINK' =>  'https://www.patreon.com/davidrevoy',
+
+################################################################################
+# Page: Webcomics
+'WEBCOMIC_EPISODE'		    =>  'Tajle kōmiksu',
+'WEBCOMIC_MAKINGOF'		    =>  'Produkcyjŏ',
+'WEBCOMIC_MAKINGOF_DESCRIPTION' =>  '
+    <p> Produkcyjŏ i <a href="http://www.davidrevoy.com/categorie5/tutorials">tutoriale</a> sōm ekstra bonusym, kery szpōnzorujōm moje <a href="https://www.patreon.com/davidrevoy">Patreōnowe &#39;bohatyry&#39;</a></p>
+',
+'WEBCOMIC_ARTWORK'		    =>  'Galeryjŏ grafik',
+'WEBCOMIC_SKETCHES'		    =>  'Cechōnki',
+
+################################################################################
+# Page: Blog
+# when content are not English (no need to warn user the content is english only here):
+#'LIMITATIONS' 	=> '<i class="fa fa-info-circle"></i> Content available in english only ',
+'LIMITATIONS' 		=> '<i class="fa fa-info-circle"></i> Treść dostympnŏ jyno we angelskij gŏdce',
+
+################################################################################
+# Page: Philosophy
+'PHILOSOPHY_CONTENT'    =>  '
+ <h2>Spyrany bez patrōnōw</h2>
+
+    <p>Projekt Pepper&amp;Carrot je fundowany bez czytŏczōw ze cołkigo świata.<br/>
+    Kŏżdy z patrōnōw przelywŏ trochã pijyndzy na nowõ tajlã, za co dostŏwŏ podziynkowanie na kōńcu kŏżdyj tajle.<br/>
+    Dziynki tymu Pepper&amp;Carrot może być niyznŏleżny i swobodny ôd zmaraszyniŏ ryklamami i marketingym.</p>
+
+    <div class="philobutton">
+        <a href="https://www.patreon.com/davidrevoy" title="Wystyknie 1$ na Patreōnie, by być patrōnym nowyj tajle">
+            Wystyknie 1$ na Patreōnie, by być patrōnym nowyj tajle
+        </a>
+    </div>
+
+        <a href="https://www.patreon.com/davidrevoy" title="Wystyknie 1$ na Patreōnie, by być patrōnym nowyj tajle">
+            <img alt="ôbrŏzek sparciŏ" src="data/images/static/2015-02-09_philosophy_01-support.jpg">
+        </a>
+
+    <h2>We 100% darmy, bez płacyniŏ</h2>
+
+    <p>Wszyjsko, co tworzã ô Pepper&amp;Carrot, je dostympne sam za darmo i dlŏ wszyjskich.<br/>
+    Mōm reszpekt dlŏ Wŏs wszyjskich: z geldym abo bez. Wszyjske bōnusy dlŏ patrōnōw idzie znojś sam.<br/>
+    Pepper&amp;Carrot nikej niy poprosi Wŏs ô płat abo zaabōniyrowanie, cobyście mogli ôbejzdrzeć nowõ treść.</p>
+
+        <img alt="ôbrŏzek płatu" src="data/images/static/2015-02-09_philosophy_03-paywall.jpg">
+
+    <h2>Ôdewrzōny i liberalny</h2>
+
+    <p>Chcã dŏć ludziom prawo do dzielyniŏ, użytku, budowaniŏ, aji zarŏbianiŏ na robocie, kerõ stworzōłch.<br/>
+    Wszyjske zajty, grafiki i treść były sprŏwiōne w ôdewrzōnym/liberalnym softwerze na GNU/Linuksie. Źrōdła dostympne sōm tukej, dostyknie ôbrać 'Źrōdła' z myni.<br/>
+    Zachyncōm do użyciŏ kōmercyjnego, przekłŏdu, tworzyniŏ fanartōw, drukowaniŏ, wyużyciŏ w filmach, szpilach wideo i ladowaniŏ na inksze zajty.<br/>
+    Dostyknie, byście pedzieli, fto je autorami (kōnsztlery, korektōry, tumacze, kerzi mieli swōj wkłŏd w dzieło), podali link do licyncyje i naporynczyli wkludzōne pōmiany. Możecie zrobić to w kŏżdy usōndny knif, jak dugo niy sugeruje ôn dŏwaniŏ bez licyncjodŏwcy sparciŏ dlŏ Ciebie abo Twojigo knifu wyużyciŏ dzieła.</p>
+
+    <div class="philobutton">
+        <a href="https://creativecommons.org/licenses/by/4.0/" title="Coby przewiedzieć sie wiyncyj, przeczytej Creative Commons Uzdanie autorstwa 4.0">
+            Licyncyjŏ: Creative Commons Uzdanie autorstwa 4.0
+        </a>
+    </div>
+
+        <img alt="ôbrŏzek, co pokŏzuje ôdewrzōne mydia" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
+
+    <h2>Wysokij jakości szpas dlŏ wszyjskich i wszyndy</h2>
+
+    <p>Pepper&amp;Carrot je uciesznym kōmiksym, przipasowanym do kŏżdego czytŏcza.<br/>
+    Niy ma w nim treści dlŏ majoryntnych ani gwołtōw. Pepper&amp;Carrot je blank ôdewrzōny, ale je tyż dumnym przikłŏdym, jak szumne mogōm być darme rzeczy.<br/>
+    Skupiōm sie na zwielgij jakości, pōniywŏż darme niy musi ôznŏczać złe abo amatorske. Blank ôpacznie.</p>
+
+        <img alt="" src="data/images/static/2015-02-09_philosophy_05-everyone.jpg">
+
+    <h2>Pōdźcie zmiyniymy branżã kōmiksu!</h2>
+
+    <p>Bez zmyńszynie wielości postrzednikōw miyndzy czytŏczami i kōnsztlerym, płacisz mynij, a jŏ dostŏwōm wiyncyj. Spōmŏgŏsz mie dyrekt.<br/>
+    Żŏdyn wydŏwca/dystrybutōr niy może prziginōńć mie do pōmiany Pepper&amp;Carrot, cobych napasowoł go do ôd niygo wizyje&#39;rynku&#39;.<br/>
+     &nbsp;.&nbsp;.&nbsp;.&nbsp;Czymu jedyn sukces niy miołby pociōngnōńć za sobōm reszty industryje we kryzysie? Ôbejzdrzimy...</p>
+
+        <img alt="ôbrŏzek: branża kōmiksu a sparcie patrōnōw" src="data/images/static/2015-02-09_philosophy_06-industry-change.jpg">
+
+    <div class="philobutton">
+        <a class="h4" href="https://www.patreon.com/davidrevoy" title="Wystyknie 1$ na Patreōnie, by być patrōnym nowyj tajle">
+            pōmōż mi we produkcyji Pepper&amp;Carrot
+        </a>
+    </div>
+
+    <p>Dziynkujã za czytanie!<br/>
+    -David Revoy</p>
+
+ ',
+
+################################################################################
+# Page: Contribute
+'CONTRIBUTE_TITLE'		=> 'Pōmōż',
+'CONTRIBUTE_TOP'		=> '
+    <p>Dziynki <a href="?static6/sources" title="Źrōdła">ôdewrzōnym źrōdłōm</a> i <a href="http://creativecommons.org/licenses/by/4.0/">
+    licyncyje creative commons</a> możesz pōmōc Pepper&amp;Carrot na moc knifōw:</p>
+',
+'CONTRIBUTE_DONATION'		=> '
+    <h2>Patrōnat, dŏwki</h2>
+
+    <p>Na zajcie ze <a href="?static2/philosophy" title="ôdewrzij zajtã filozofije">filozofijōm</a> wyeklerowołch bezma wszyjsko ô financowaniu.<br/>
+    Patrōnym Pepper&amp;Carrot idzie być za 1$ <a href="https://www.patreon.com/davidrevoy">na Patreōnie</a>.<br/>
+    Patreon akceptuje szkarty borgowe z cołkigo świata, a tyż płaty bez Paypal.<br/>
+            Możesz tyż spōmōc mie dyrekt bez Paypal abo Flattr:</p>
+',
+'CONTRIBUTE_FANART'	    => '
+    <h2>Ôd fanōw:</h2>
+    <p>Pepper&amp;Carrot je ôdewrzōny na fanōw: cechōnki, scynariusze, rzeźby, modele 3D, fan-fiction. Prześlij mi je, (<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>, abo napisz na placu społecznościowym) coby trefiyły do galeryje fanartōw:</p>
+',
+'CONTRIBUTE_DERIVATIONS' => '
+    <h2>Adaptacyje:</h2>
+    <p>Pepper&amp;Carrot może być adaptowany do wielu projektōw i produktōw. Możno sprŏwisz włŏsny abo spōmożesz kogoś?</p>
+',
+
+'CONTRIBUTE_BOTTOM'	    => '
+    <h2>Przekłŏd i korekta:</h2>
+
+    <p>Plac Pepper&amp;Carrot je wielogodkowy i akceptuje kożdõ z nich (aji wymarłe i fikcyjne). Źrōdła ôd zajty sōm dostympne do przekłŏdu. Przeczytej <a href="index.php?fr/article267/translation-tutorial">instrukcyjõ</a>, coby przewiedzieć sie, jak przidać włŏsny przekłŏd.</p>
+
+    <h2>Presa, nece społecznościowe</h2>
+
+    <p>Bydź wydŏwcōm Pepper&amp;Carrot! Pisz artikle, rychtuj posty, tajluj sie i ôdewiyrej skupiny na ôblubiōnych necach społecznościowych. Na zajcie źrōdeł możesz sebrać <a href="?static6/sources" title="Źródła">sztofy presowe</a>.</p>
+
+    <h2>Inksze idyje na wkłŏd&nbsp;.&nbsp;.&nbsp;.&nbsp;</h2>
+
+    <p>Kożdy może pōmŏgać jak chce:<br/>
+        <b>Dewelopery:</b>  zrychtowanie czytnika na mobilne masziny.<br />
+        <b>Muzyki:</b>  kōmpōnowanie tymatōw dlŏ Pepper&amp;Carrot.<br />
+        <b>Pisŏrze:</b>  forszlagi nowych scynariuszōw.<br />
+        <b>Dziynnikŏrze:</b> informiyrowanie o Pepper&amp;Carrot we tradycyjnych mydiach (cajtōngi, telewizyjŏ atd.).<br />
+        <b>Drukŏrze:</b> drukowanie plakŏtōw i inkszych dobrot ze Pepper&amp;Carrot.<br />
+    </p>
+
+    <h2>Kanał IRC:</h2>
+
+    <p>Ôzprŏwiej ô Pepper&amp;Carrot. Je żech sam w dziyń (czasu europejskigo) (nick: deevad)<br/>
+',
+
+################################################################################
+# Page: Sources
+'SOURCES_TITLE'		    => 'Źrōdła',
+'SOURCES_TOP'	        => '
+    <p><b>Witej w cyntrōm sebiyraniŏ źrōdeł!</b><br/>
+    Znojdziesz sam zbiory źrōdłowe (i niy ino) Pepper&amp;Carrot.<br/>
+    Wszyjske zbiory graficzne sōm kōmpatybilne z nŏjnowszōm wersyjōm <a href="https://krita.org/">Krity</a>.</p>
+',
+'SOURCES_BOTTOM'	    => '
+    <p>Bez sebiyranie i robotã na tych zbiorach, zgŏdzŏsz sie ze prawidłami licyncyje<br/>
+    <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Uzdōnie autorstwa</a>.
+    Wejzdrzij do zbiorōw README, coby przewiedzieć sie wiyncyj.</p>
+
+    <p><b><i class="fa fa-github" style="font-size:1.5em;" ></i> Repozytoria przekłŏdōw, skryptōw, placu necowego, grafik atd.:</b><br/></p>
+',
+'SOURCE_COVER'   =>  'Ôbkłŏdki',
+'SOURCE_KRITA'   =>  'Źrōdłowe zbiory dlŏ Krity',
+'SOURCE_TRANSLATOR'=>  'Kōmplet przekłŏdŏcza',
+'SOURCE_WEB'      =>  '<span class="sourceinfos">Leke zbiory JPG dlŏ strōn (90ppi)</span>',
+'SOURCE_PRINT'        =>  '<span class="sourceinfos">Bezstratne zbiory do druku (300ppi)</span>',
+'SOURCE_MONTAGE'   =>  '<span class="sourceinfos">Wszyjske panele na jednyj strōnie</span>',
+
+################################################################################
+# Page: Author
+'AUTHOR_TITLE'		=> 'Ô Davidzie Revoyu',
+'AUTHOR_BIO'	    => '
+    <p>Serwus, mianujã sie David Revoy i je żech francuskim kōnsztlerym narodzōnym we 1981 roku. Je żech samoukym, co pasjōnuje sie cechōnkym, malyrkōm, kotami, kōmputrami, kulturōm ôdewrzōnych źrōdeł GNU/Linuksa, Internetym, starymi szpilami RPG, mangami i anime, kōnsztym tradycyjnym, japōńskōm kulturōm i fantastykōm&nbsp;.&nbsp;.&nbsp;.&nbsp;<br />
+    <br />
+    Po pōnad dziesiyńciu latach digitalnyj malyrki, wynauczaniŏ, tworzyniŏ cechōnkōw kōncepcyjnych i ilustracyji i kerowaniŏ robotami uzdołch napoczōńć włŏsny projekt. W kōńcu znŏdłech knif zmiynszaniŏ wszyjskich mojich spodobań; rezultatym je Pepper&amp;Carrot.</p>
+
+    <p>moje portfolio:</p>
+',
+'AUTHOR_TODO_DREAM'	=> '
+    <h3>Lista marzyń:</h3>
+
+    <p>
+    ☐ Pedzieć ô Pepper&amp;Carrot na japōńskim kōnwyncie kōmiksowym<br/>
+    ☐ Poszpilać na padzie we szpil ô Pepper&amp;Carrot<br/>
+    ☐ Mieć galeryjõ ze 100 fanartami<br/>
+    ☑ <a href="http://www.peppercarrot.com/en/article376/dream-to-do-list-a-wikipedia-page">Mieć strōnã na Wikipedyji</a><br/>
+    ☐ Dostać fotografijõ cosplayu Pepper<br/>
+    ☑ <a href="http://www.peppercarrot.com/article304/dream-to-do-list-a-cat-named-carrot">Dostać fotografijõ ryżygo kota mianowanego Carrot</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article302/peppercarrot-has-over-500-supporters">Dostać sparcie ôd 500 patrōnōw</a><br/>
+    ☐ Wydać 100 tajlōw<br/>
+    </p>
+',
+
+'AUTHOR_CARREER_TITLE'				 => 'moja kariera w 7 bōmbelkach:',
+'AUTHOR_CARREER_BUBBLE_DESCRIPTIONS' => '<p></p>',
+
+
+################################################################################
+# Page: Wiki
+'WIKI'		=> 'Wiki',
+
+################################################################################
+# Website General: Footer
+'FOOTER_CONTENT'    => '
+    <p>Kōmiks, ôbrŏzki i teksty sōm na licyncyji <a href="https://creativecommons.org/licenses/by/4.0/" title="Coby przewiedzieć sie wiyncyj, przeczytej Creative Commons Uzdanie autorstwa 4.0">Creative Commons Uzdanie autorstwa 4.0</a>, jeźli strōna niy gŏdŏ inakszyj.<br/>
+    Uzdanie autostwa "David Revoy, www.davidrevoy.com". Napisz dō mie: <a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>, coby przewiedzieć sie wiyncyj.</p>
+
+    <p>Plac podpiyrany bez <a href="http://www.pluxml.org" title="PlXml">PluXml</a></p>
+',
+
+################################################################################
+# various utils:
+'UTIL_NEXT_EPISODE'      =>  'dalszŏ tajla',
+'UTIL_PREVIOUS_EPISODE'  =>  'piyrwyjszŏ tajla',
+'UTIL_EPISODE'   =>  'tajla',
+'UTIL_BACK_TO_GALLERY'      =>  'nazŏd do galeryje',
+'UTIL_MORE'      =>  'wiyncyj',
+'UTIL_PAGE'      =>  'strōna',
+'UTIL_BY'        =>  'bez',
+'UTIL_LICENSE'   =>  'licyncyjŏ',
+'FIRST'        =>  'piyrszŏ',
+'LAST'   =>  'ôstatniŏ',
+);
+
+?>
