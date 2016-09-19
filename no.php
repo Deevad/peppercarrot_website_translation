6d454fe91e4adf677c92507899d1cde5455b97f3
David Revoy <info@davidrevoy.com>
2016-09-19 22:38:58 +0100

    [no] Norwegian translation , thanks Thomas Nordstrøm

---
+++ no.php
@@ -0,0 +1,270 @@
+<?php
+
+$LANG = array(
+
+# Global
+'TRANSLATED_BY' 		    => 'Oversatt av: <a href="https://3dnett.no">Thomas Nordstrøm</a>',
+'LANGUAGE_NAME' 			=> 'Norsk',
+'LANGUAGE_ISO_CODE_2_LETTER'=> 'no',
+# http://www.w3schools.com/tags/ref_language_codes.asp --> full list
+
+################################################################################
+# Header HTML infos for search engine and title in tab:
+'PEPPERCARROT_VEGETABLE'    =>  'Pepper og Gulrot', // litteral translation of spices + vegetable
+'Website_DESCRIPTION'        =>  'Pepper&amp;Gulrot sin offisielle hjemmeside, en fri/libre og &aring;pen kildekode nettstripe om Pepper, en ung heks og katten hennes, Gulrot. De lever i en fantasiverden med trylledrikker, magi, og overnaturlige skapninger.',
+'SUBTITLE'                  =>  'Den frie nettstripen av David Revoy',
+
+################################################################################
+# Top menu website:
+'HOMEPAGE'      =>  'Hjemmeside',
+'WEBCOMICS'     =>  'Nettstriper',
+'BLOG'          =>  'Blogg',
+'PHILOSOPHY'    =>  'Filosofi',
+'CONTRIBUTE'    =>  'Bidra',
+'SOURCES'       =>  'Kildefiler',
+'AUTHOR'        =>  'Forfatter',
+'FOLLOW'        =>  'F&oslash;lg Pepper&amp;Gulrot p&aring;:',
+
+################################################################################
+# Top and bottom translation panel
+'ADD_TRANSLATION'       => 'legg til oversettelse',
+'CORRECTIONS' 			=> 'foresl&aring; rettelser',
+
+################################################################################
+# Page: Homepage
+'HOMEPAGE_BIG_TEXT'		    =>  '
+    En fri, libre og &aring;pen kildekode nettstripe<br/>
+    st&oslash;ttet direkte av "Patreon"-f&oslash;lgerne<br/>
+    i h&aring;p om &aring; forandre tegneserieindustrien!<br/>
+',
+'HOMEPAGE_PATREON_INFO'     =>  'Kun 1$ per nye episode, bli en sponsor idag på Patreon',
+'HOMEPAGE_MOREINFO_BUTTON'  =>  'Mer informasjon',
+'HOMEPAGE_PATREON_BUTTON'   =>  'bli en Pepper&amp;Gulrot sponsor på Patreon',
+'HOMEPAGE_LAST_EPISODE'     =>  'Siste episode:',
+'HOMEPAGE_NEWS_UPDATE'      =>  'Nyheter &amp; oppdateringer:',
+'HOMEPAGE_MOREPOSTS_BUTTON' =>  'Flere innlegg',
+'HOMEPAGE_MAINSERVICE_LINK' =>  'https://www.patreon.com/davidrevoy',
+
+################################################################################
+# Page: Webcomics
+'WEBCOMIC_EPISODE'		    =>  'Nettstripe episoder',
+'WEBCOMIC_MAKINGOF'		    =>  'Hvordan den lages',
+'WEBCOMIC_MAKINGOF_DESCRIPTION' =>  '
+    <p> Hvordan den lages <a href="http://www.davidrevoy.com/categorie5/tutorials">art tutorials</a> er ekstramateriale sponset av <a href="https://www.patreon.com/davidrevoy">Patreons &#39;heroes&#39;</a></p>
+',
+'WEBCOMIC_ARTWORK'		    =>  'Galleri',
+'WEBCOMIC_SKETCHES'		    =>  'Skisser',
+
+################################################################################
+# Page: Blog
+# when content are not English (no need to warn user the content is english only here):
+#'LIMITATIONS' 	=> '<i class="fa fa-info-circle"></i> Content available in english only ',
+'LIMITATIONS' 		=> '<i class="fa fa-info-circle"></i> innhold er kun tilhjengelig p&aring; engelsk ',
+
+################################################################################
+# Page: Philosophy
+'PHILOSOPHY_CONTENT'    =>  '
+ <h2>Støttet av sponsorer</h2>
+
+    <p>Pepper&amp;Gulrot er finansiert ene og alene av sponsorer verden over.<br/>
+	Hver sponsor sender en liten slant med penger for hver nye episode som blir laget og blir kreditert på slutten av hver episode.<br/>
+	Dette systemet tillater Pepper&amp;Gulrot &aring; v&aelig;re 100 prosent uavhengig uten &aring; måtte ty til reklame eller andre triks som kan
+	ødelegge seriens orginalitet.</p>
+
+    <div class="philobutton">
+        <a href="https://www.patreon.com/davidrevoy" title="Bli en Pepper&amp;Gulrot sponsor idag for bare 1$">
+            Bli en Pepper&amp;Gulrot sponsor idag for bare 1$ per episode.
+        </a>
+    </div>
+
+        <a href="https://www.patreon.com/davidrevoy" title="Bli en Pepper&amp;Gulrot sponsor idag for bare 1$ per episode">
+            <img alt="illustration representing patronage" src="data/images/static/2015-02-09_philosophy_01-support.jpg">
+        </a>
+
+    <h2>100% gratis, for alltid, ingen betalingsmur</h2>
+
+    <p>Alt epper&amp;gulrot materiale jeg produserer er tilgjengelig p&aring; denne nettsiden fritt og tilgjengelig for alle.<br/>
+	Jeg har like mye respekt for dere alle enten dere bidrar eller ikke. Alt ekstramateriale jeg lager for mine sponsorer blir ogs&aring; gjort tilgjengelig her.<br/>
+    Pepper&amp;Gulrot kommer aldri til å be deg om hverken penger eller noen form for abonnement for &aring; f&aring; tilgang til nytt innhold.</p>
+
+        <img alt="illustration representing paywall" src="data/images/static/2015-02-09_philosophy_03-paywall.jpg">
+
+    <h2>Åpent og givende</h2>
+
+    <p>Jeg ønsker å gi alle full rett til å dele, bruke, endre og tilogmed tjene penger på materialet jeg har produsert.<br/>
+	Alle sider og alt innhold er laget med fri/libre åpen kildekode programmer på GNU/Linux. Alt kildemateriale er tilgjengelig her, &#39;Kildefiler&#39; i menyen.<br />
+	Jeg oppmuntrer til kommersiel bruk, fan-art, utskrifter, filmer, videospill og videredeling<br/>
+	Det eneste jeg vil ha tilbake er kreditering av skaperne (Kunstnere, korrekturlesere og oversettere involvert i materialet du &oslash;nsker &aring; bruke).
+	Legg ved lenke til lisensen og formidle om det har blitt gjort endringer. Dette b&oslash;r gj&oslash;res p&aring;
+	en fornuftig m&aring;te, men uten &aring; antyde at forfatterne har hatt noe med deg eller din bruk &aring; gj&oslash;re.</p>
+    
+
+    <div class="philobutton">
+        <a href="https://creativecommons.org/licenses/by/4.0/" title="For mer informasjon, les Creative Commons Attribution 4.0">
+            Lisens: Creative Commons Attribution 4.0
+        </a>
+    </div>
+
+        <img alt="illustration representing open-source media" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
+
+    <h2>Kvalitetsunderholdning for alle over alt</h2>
+
+    <p>Pepper&amp;Gulrot er en humoristisk nettstripe myntet p&aring; voksne og barn i alle aldre.<br/>
+    Fritt for upassende innhold. Den &aring;pne nettstripen Pepper&amp;Gulrot er et stolt eksempel p&aring; hvor bra fri og &aring;pen kultur kan v&aelig;re.<br/>
+	
+	Jeg har veldig fokus p&aring; kvalitet for &aring; vise at fritt og åpent ikke betyr d&aring;rlig eller amat&oslash;rmessig. Au contraire.</p>
+
+        <img alt="" src="data/images/static/2015-02-09_philosophy_05-everyone.jpg">
+
+    <h2>La oss endre tegneserieindustrien!</h2>
+
+    <p>
+	Ettersom det ikke er noe mellomledd mellom tegner og publikum, betaler du mindre og f&aring;r mer. Din st&oslash;tte g&aring;r til meg direkte.</br>
+	Ingen utgivere distribut&oslash;rer eller markedsf&oslash;rere kan tvinge meg til &aring; forandre Pepper&amp;Gulrot for &aring; tilpasses til deres markedsvisjon.</br>
+	Er der mulig for en enslig seriesuksess &aring; skape en sn&oslash;balleffekt som forandrer en industri i krise? Det gjenst&aring;r &aring; se...</p>
+	
+        <img alt="illustration: comic book industry VS patreon support" src="data/images/static/2015-02-09_philosophy_06-industry-change.jpg">
+
+    <div class="philobutton">
+        <a class="h4" href="https://www.patreon.com/davidrevoy" title="Bli en Pepper&amp;Gulrot sponsor for kun 1$ per nye episode">
+            hjelp meg &aring; &oslash;ke produksjonen av Pepper&amp;Gulrot idag.
+        </a>
+    </div>
+
+    <p>Takk for at du tok deg tid til &aring; lese dette! <br/>
+    -David Revoy</p>
+
+ ',
+
+################################################################################
+# Page: Contribute
+'CONTRIBUTE_TITLE'		=> 'Bidra',
+'CONTRIBUTE_TOP'		=> '
+    <p>Takket v&aelig;re <a href="?static6/sources" title="Sources page">&aring;pen kildekode</a> og <a href="http://creativecommons.org/licenses/by/4.0/">
+    creative commons lisensene</a> kan du bist&aring; Pepper&amp;Gulrot p&aring; mange m&aring;ter:</p>
+',
+'CONTRIBUTE_DONATION'		=> '
+    <h2>Donasjoner fra f&oslash;lgere</h2>
+
+    <p>P&aring; <a href="?static2/philosophy" title="open the Sources page">Filosofi-siden</a> kan du lese i detaljer det meste som ang&aring;r finansiering av serien.<br/>
+	Det er en smal sak &aring; bli en Pepper&amp;Gulrot-sponsor og det for bare 1$ per nye episode. G&aring; til<a href="https://www.patreon.com/davidrevoy">Patreon</a>.<br/>
+	Patreon godtar kredittkort fra hele verden s&aring; vel som betaling via Paypal.<br/>
+	Jeg tar ogs&aring; imot donasjoner direkte via Paypal eller Flattr:</p>
+
+',
+'CONTRIBUTE_FANART'	    => '
+    <h2>Fan-art</h2>
+    <p>Pepper&amp;Gulrot setter stor pris p&aring; fan-art som: tegninger, scenarier, skulpturer, 3D modeller, fan-fiction. Send dem til meg (<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>, eller poke meg p&aring; sosiale nettverk) for &aring; bli med i fan-art galleriet:</p>
+',
+'CONTRIBUTE_DERIVATIONS' => '
+    <h2>Avarter:</h2>
+    <p>Pepper&amp;Gulrot kan tilpasses til mange prosjekter eller produkter, hvorfor ikke lage noe selv eller bidra til eksisterende prosjekter?</p>
+',
+
+'CONTRIBUTE_BOTTOM'	    => '
+    <h2>Oversettelser eller korrekturlesning:</h2>
+
+    <p>Pepper&amp;Gulrot er laget for &aring; v&aelig;re flerspr&aring;klig og aksepterer alle spr&aring;k (inkludert utd&oslash;dde og oppdiktede). Kildefilene til denne siden ligger klart til alle som &oslash;nsker &aring;
+	oversette. Se <a href="index.php?fr/article267/translation-tutorial">reference tutorial</a> for mere informasjon om hvordan du kan legge til din oversettelse.</p>
+	
+
+    <h2>Presse, og sosiale medier</h2>
+
+	<p>Bli en utgiver av Pepper&amp;Gulrot! Skriv artikler, lag blogginnlegg, del og skap engasjement p&aring; dine foretrukne sosiale nettverk.
+	Du kan laste ned <a href="?static6/sources" title="Sources page">pressepakker</a> p&aring; "kildefiler"-siden.</p>
+  
+
+    <h2>Andre m&aring;ter &aring; bidra p&aring;&nbsp;.&nbsp;.&nbsp;.&nbsp;</h2>
+
+    <p>Alle kan bidra p&aring; ulike m&aring;ter:<br/>
+        <b>Utvikler:</b> Lag en mobilapp sS&aring; en kan lese Pepper&amp;Gulrot p&aring; mobil eller nettbrett.<br/>
+        <b>Musiker:</b> Lage musikkstykker til Pepper&amp;Gulrot.<br/>
+        <b>Forfatter:</b> Foresl&aring; nye Pepper&amp;Gulrot historier.<br/>
+        <b>Journalist:</b> Informere tradisjonelle medier om Pepper&amp;Gulrot (aviser, tv, etc.)<br/>
+        <b>Grafiker:</b> Lage plakater eller annet materiel med Pepper&amp;Gulrot tema.<br/>
+    </p>
+
+    <h2>IRC kanal:</h2>
+
+    <p>Chat og diskuter Pepper&amp;Gulrot. Jeg vil v&aelig;re tilgjengelig p&aring; europeisk tid (kallenavn:deevad)<br/>
+',
+
+################################################################################
+# Page: Sources
+'SOURCES_TITLE'		    => 'R&aring;materiale',
+'SOURCES_TOP'	        => '
+    <p><b>Velkommen til nedlastingssenter for r&aring;filer!</b><br/>
+	Her finner du Pepper&amp;Gulrots kildefiler.<br/>
+    Alle digitale tegninger vil v&aelig;re tilgjengelig her, kompatibel med siste versjon av <a href="https://krita.org/">Krita</a>.</p>
+
+',
+'SOURCES_BOTTOM'	    => '
+    <p>N&aring;r du laster ned og jobber med disse filene m&aring; dette gj&oslash;res i tr&aring;d med<br/>
+    <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution lisensen</a>.
+    For mer info kan du lese README filene tilh&oslash;rende hvert prosjekt.</p>
+    <p><b><i class="fa fa-github" style="font-size:1.5em;" ></i> Kodebr&oslash;nner for oversettelser, skript. nettside, grafikk,etc.:</b><br/></p>
+',
+'SOURCE_COVER'   =>  'Bladomslag',
+'SOURCE_KRITA'   =>  'Krita kildefiler',
+'SOURCE_TRANSLATOR'=>  'Pakker for oversettere',
+'SOURCE_WEB'      =>  '<span class="sourceinfos"> 90ppi, lettvekts jpg </span>for nett ',
+'SOURCE_PRINT'        =>  'Print <span class="sourceinfos"> 300ppi, tapsfri kvalitet </span>',
+'SOURCE_MONTAGE'   =>  'Montasjer <span class="sourceinfos"> Alle paneler p&aring; en side </span>',
+
+################################################################################
+# Page: Author
+'AUTHOR_TITLE'		=> 'Om David Revoy',
+'AUTHOR_BIO'	    => '
+
+    <p>Hei, jeg er David Revoy, en selvl&aelig;rt fransk kunstner f&oslash;dt i 1981. Jeg er selvl&aelig;rt med lidenskap for tegning, maling, katter, datamaskiner, Gnu/Linux åpen kildekode kultur, internett, "old school RPG" dataspill, gamle mangas og anime, tradisjonell kunst, japansk kultur &amp; eventyr&nbsp;.&nbsp;.&nbsp;.&nbsp;<br/>
+    <br/>
+    Etter mer enn 10 &aring;r som freelancer innen digital-maling, l&aelig;ring og konsepttegning, bestemte jeg meg for &aring; starte mitt eget prosjekt. Jeg fant til slutt en m&aring;te &aring; involvere alle mine lidenskaper gjennom Pepper&amp;Gulrot serien.</p>
+
+    <p>Min portef&oslash;lge:</p>
+',
+'AUTHOR_TODO_DREAM'	=> '
+    <h3>Dr&oslash;mme-TO-DO liste:</h3>
+
+    <p>
+    ☐ Tale p&aring; en japansk tegneseriekonferanse om Pepper&amp;Gulrot<br/>
+    ☐ Spille et Pepper&amp;Gulrot spill p&aring; en spillkonsoll<br/>
+    ☐ F&aring; over 100 fan-arts i galleriet<br/>
+    ☑ <a href="http://www.peppercarrot.com/en/article376/dream-to-do-list-a-wikipedia-page">F&aring; en egen Wikipedia side</a><br/>
+    ☐ Motta bilde av en Pepper cosplay<br/>
+    ☑ <a href="http://www.peppercarrot.com/article304/dream-to-do-list-a-cat-named-carrot">F&aring; et bilde av en r&oslash;d kat som heter Gulrot</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article302/peppercarrot-has-over-500-supporters">bli st&oslash;ttet av 500 f&oslash;lgere p&aring; Patreon</a><br/>
+    ☐ N&aring; episode 100<br/>
+    </p>
+',
+
+'AUTHOR_CARREER_TITLE'				 => 'min karriere i 7 tegneseriebobler:',
+'AUTHOR_CARREER_BUBBLE_DESCRIPTIONS' => '<p></p>',
+
+################################################################################
+# Page: Wiki
+'WIKI'		=> 'Wiki',
+
+################################################################################
+# Website General: Footer
+'FOOTER_CONTENT'    => '
+    <p>Nettstriper, h&aring;ndtverk og tekster er lisensiert under en<a href="https://creativecommons.org/licenses/by/4.0/" title="For more information, read the Creative Commons Attribution 4.0">Creative Commons Attribution 4.0</a> lisens, med mindre annet er nevnt.<br/>
+    krediteres "David Revoy, www.davidrevoy.com". Kontakt meg: <a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a> for mer informasjon.</p>
+
+    <p>Website powered by <a href="http://www.pluxml.org" title="PlXml">PluXml</a></p>
+',
+
+################################################################################
+# various utils:
+'UTIL_NEXT_EPISODE'      =>  'neste episode',
+'UTIL_PREVIOUS_EPISODE'  =>  'forrige episode',
+'UTIL_EPISODE'   =>  'episode',
+'UTIL_BACK_TO_GALLERY'      =>  'tilbake til galleriet',
+'UTIL_MORE'      =>  'mer',
+'UTIL_PAGE'      =>  'side',
+'UTIL_BY'        =>  'av',
+'UTIL_LICENSE'   =>  'lisens',
+'FIRST'        =>  'f&oslash;rste',
+'LAST'   =>  'siste',
+);
+
+?>
