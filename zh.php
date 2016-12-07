058fedd0c0b869bc8d6a74402282ef8ba9d026f8
Biniou180 <thouvenot.pyves+git@gmail.com>
2016-12-07 16:23:49 +0100

    Add_CN_web_translation

---
+++ zh.php
@@ -0,0 +1,259 @@
+<?php
+
+$LANG = array(
+
+# Global
+'TRANSLATED_BY' 		=> '翻译:庄冉',
+'LANGUAGE_NAME' 			=> '简体中文',
+'LANGUAGE_ISO_CODE_2_LETTER'=> 'zh',
+# http://www.w3schools.com/tags/ref_language_codes.asp --> full list
+
+################################################################################
+# Header HTML infos for search engine and title in tab:
+'PEPPERCARROT_VEGETABLE'    =>  '辣椒和胡萝卜', // litteral translation of spices + vegetable
+'Website_DESCRIPTION'       =>  '小辣椒与萝卜头官方网站，一部自由的网络漫画，开源并且免费。讲述了一名生活在魔药，魔法和神奇生物的世界里的少女和她的猫的故事。',
+'SUBTITLE'                  =>  'David Revoy的自由漫画',
+
+################################################################################
+# Top menu website:
+'HOMEPAGE'      =>  '主页面',
+'WEBCOMICS'     =>  '网络漫画',
+'BLOG'          =>  'Blog',
+'PHILOSOPHY'    =>  '理念',
+'CONTRIBUTE'    =>  '贡献者',
+'SOURCES'       =>  '源文件',
+'AUTHOR'        =>  '作者',
+'FOLLOW'        =>  '小辣椒与萝卜头的社交足迹：',
+
+################################################################################
+# Top and bottom translation panel
+'ADD_TRANSLATION'       => '添加一种新语言',
+'CORRECTIONS' 			=> '修改建议',
+
+################################################################################
+# Page : Homepage
+'HOMEPAGE_BIG_TEXT'		    =>  '
+    一部自由的网络漫画，开源并且免费。<br/>
+    由读者提供资金支持，无中介商家。<br/>
+    一部力求改善漫画产业链现状的连环画！<br/>
+',
+'HOMEPAGE_PATREON_INFO'     =>  '只要1美元/集，就可以成为小辣椒与萝卜头的赞助者。',
+'HOMEPAGE_MOREINFO_BUTTON'  =>  '更多信息',
+'HOMEPAGE_PATREON_BUTTON'   =>  '成为小辣椒与萝卜头的赞助者',
+'HOMEPAGE_LAST_EPISODE'     =>  '最新一集：',
+'HOMEPAGE_NEWS_UPDATE'      =>  '新闻及更新：',
+'HOMEPAGE_MOREPOSTS_BUTTON' =>  '查看更多',
+'HOMEPAGE_MAINSERVICE_LINK' =>  'https://www.patreon.com/davidrevoy',
+
+################################################################################
+# Page : Webcomics
+'WEBCOMIC_EPISODE'		    =>  '漫画集',
+'WEBCOMIC_MAKINGOF'		    =>  '制作过程Making-of',
+'WEBCOMIC_MAKINGOF_DESCRIPTION' =>  '
+    <p>制作过程和<a href="http://www.davidrevoy.com/categorie5/tutorials">美工教程</a>的项目基金是由赞助者额外给予的。具体请参见<a href="https://www.patreon.com/davidrevoy">Patreons网站</a></p>
+',
+'WEBCOMIC_ARTWORK'		    =>  '画廊',
+'WEBCOMIC_SKETCHES'		    =>  '素描',
+
+################################################################################
+# Page : Blog
+# An empty one in English (no need to warn user the content is english only here) :
+'LIMITATIONS' 	=> '本页内容只提供英语版本',
+
+################################################################################
+# Page : Philosophy
+'PHILOSOPHY_CONTENT'    =>  '
+ <h2>由读者提供资金支持</h2>
+    
+    <p>小辣椒与萝卜头项目是由全世界的读者提供经济支持的。<br/>
+    所有读者都可以以一集漫画为单位给予一小笔赞助。<br/>
+    这些给予赞助的读者将会被列在该集漫画的鸣谢名单中。由于此系统的健康运作，小辣椒与萝卜头<br/>
+    得以继续以自由独立漫画的形式存在从而摆脱漫画产业，广告和商业运作的影响。</p>
+    
+    <div class="philobutton">
+        只要1美元/集，从今天开始成为小辣椒与萝卜头的赞助者，具体请参见<a href="https://www.patreon.com/davidrevoy" title="只要1$/集，通过Patreon网站成为小辣椒与萝卜头的赞助者">Patreon网站</a>.
+    </div>
+    
+        <a href="https://www.patreon.com/davidrevoy" title="只要1$一集，通过Patreon网站成为小辣椒与萝卜头的赞助者">
+            <img alt="Illustration représentant le mécénat moderne" src="data/images/static/2015-02-09_philosophy_01-support.jpg">
+        </a>
+
+    <h2>永远100%免费</h2>
+    
+    <p>我在本网站创建的一切关于小辣椒与萝卜头及其周边产品，均为免费，且可以被所有人使用。<br/>
+    我对我的读者们给予同等尊重：无论其是不是赞助者。我在此网站为赞助者所创建的额外产品也均如此。<br/>
+    小辣椒与萝卜头永远不需要您支付资金或者注册任何账户而获取漫画内容及产品资源。</p>
+    
+        <img alt="Illustration représentant les péages du net" src="data/images/static/2015-02-09_philosophy_03-paywall.jpg">
+
+    <h2>开源并且非常宽容</h2>
+    
+    <p>我希望可以给予任何人使用，交换，甚至是用于商业用途的小辣椒与萝卜头的权利。<br/>
+    本站全部页面，绘图和其他内容都是使用自由软件（Gnu/Linux）创作的，并且所有的源文件都可以在本网站找到（菜单\源文件）。<br/>
+    我真心鼓励大家利用这些素材进行商业创作，翻译，粉丝作品，出版物，电影，电子游戏以及在其他网站分享。<br/>
+    我只要求一点，那就是在作品中提及其参与创作者（作者，校对，翻译等所有为作品做出贡献的人），并且提供版权链接，<br/>
+	同时指出作品是否是被修改过。您可以合理地提供这些信息，但不可以有任何暗示您的创作行为与原作者有关或是被原作者协助。</p>
+    
+    <div class="philobutton">
+        <a href="https://creativecommons.org/licenses/by/4.0/deed.zh" title="更多信息请详见Creative Commons Attribution 4.0">
+            版权协议：Creative Commons Attribution 4.0
+        </a>
+    </div>
+
+        <img alt="Illustration représentant les médias open-sources" src="data/images/static/2015-02-09_philosophy_04-open-source.jpg">
+
+    <h2>高质量的娱乐，给任何人，在任何地方</h2>
+    
+    <p>小辣椒与萝卜头是适合任何年龄读者的轻松幽默的网络漫画。<br/>
+    这里不会涉及任何暴力及色情内容。免费且开源的小辣椒与萝卜头<br/>
+    希望成为自由媒体的一个榜样。作品重点强调品质，道德和专业精神，<br/>
+    因为“自由与开源”并不等同于“廉价和敷衍”，恰恰相反。</p>
+
+        <img alt="Illustration pour tous" src="data/images/static/2015-02-09_philosophy_05-everyone.jpg">
+
+    <h2>让我们一起来改变漫画产业！</h2>
+    
+    <p>减少作者和读者之间的各种中介是一件人人受益的事情，您的资金直接资助了作者而不是其他人。<br/>
+    同时不会有任何出版商，发行商或其他商业中介以“市场”的名义来左右小辣椒与萝卜头的创作理念。<br/>
+    为什么小辣椒与萝卜头不可以开启一个漫画产业的新时代呢？就让我们大胆的尝试一下吧！</p>
+    
+        <img alt="Illustration: l\'industrie de la BD VS financement par mécénat" src="data/images/static/2015-02-09_philosophy_06-industry-change.jpg">
+    
+    <div class="philobutton">
+        <a class="h4" href="https://www.patreon.com/davidrevoy" title="只要1$一集，通过Patreon网站成为小辣椒与萝卜头的赞助者">
+            从今天起帮助我加速小辣椒与萝卜头的成长吧！
+        </a>
+    </div>
+            
+    <p>谢谢您的关注！<br/>
+    David Revoy</p>
+ 
+ ',
+ 
+################################################################################
+# Page : Contribute 
+'CONTRIBUTE_TITLE'		=> '贡献者',
+'CONTRIBUTE_TOP'		=> '
+    <p>借助于<a href="?static6/sources" title="Sources page">源文件</a>的分享及其<a href="http://creativecommons.org/licenses/by/4.0/">
+    Creative Commons Attribution</a>发行许可，您可以通过多种途径为小辣椒与萝卜头做出贡献：</p>
+',
+'CONTRIBUTE_DONATION'		=> '           
+    <h2>赞助和捐助</h2> 
+    
+    <p>您可以在<a href="?static2/philosophy" title="Philosophie">理念</a>页面找到所有有关小辣椒与萝卜头的资助信息。<br/>
+    成为小辣椒与萝卜头的赞助者很容易，只要在<a href="https://www.patreon.com/davidrevoy">Patreon</a>网站上支付1美元/集就可以了。<br/>
+    Patreon接受全球的信用卡，您也可以在Patreon上使用Paypal账户。<br/>
+            我同时接受来自于PayPal或者Flattr的直接捐助：</p>
+',
+'CONTRIBUTE_FANART'	    => '
+    <h2>粉丝作品</h2>
+    <p>小辣椒与萝卜头接受各种形式的粉丝作品：绘画，脚本，雕塑，3D，文学作品等等……您都可以发给我，我超级喜欢：
+    <br/>（<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>，或者在社交网络上提及我）。</p>
+',
+'CONTRIBUTE_DERIVATIONS' => '
+    <h2>周边产品</h2>
+    <p>小辣椒与萝卜头可以被用来创作各种周边产品或项目（甚至是商业项目），为什么您不也来试试呢？<br/>
+    看看这里展示的有关漫画的周边产品的成品吧。</p>
+',
+
+'CONTRIBUTE_BOTTOM'	    => '
+    <h2>翻译与校对</h2>
+    
+    <p>小辣椒与萝卜头网站为多语言设计（甚至是虚拟语言或者绝迹的语言）。网页的源文件为任何翻译者或校对者开放。在<a href="index.php?fr/article267/translation-tutorial">教程</a>页面里可以找到如何添加新语言以及如何优化已有翻译文本等详细信息。</p>
+            
+    <h2>社交网络及出版物</h2> 
+    
+    <p>成为小辣椒与萝卜头的出版商，发表有关的商业文章，分享网页，建立您本人的社区团体。您可以在“源文件”页面内找到zip压缩格式的文档，包括高分辨率的logo和绘图文件。文件归类整理在<a href="?static6/sources" title="Sources page">出版工具</a>下。</p>  
+    
+    <h2>其它参与方式</h2>
+    
+    <p>众人拾柴火焰高：<br/>
+        <b>开发者：</b>使用小辣椒与萝卜头创作软件及应用。<br />
+        <b>音乐制作人：</b>为小辣椒与萝卜头系列创作主题音乐。<br />
+        <b>作家：</b>提供小说或者新的小辣椒与萝卜头漫画脚本。<br />
+        <b>记者：</b>在传统发行物（报刊，电视等）上报道小辣椒与萝卜头的相关信息。<br />
+        <b>印刷商：</b>印制小辣椒与萝卜头主题海报或周边。<br />
+    </p> 
+            
+    <h2>交流频道</h2>
+    
+    <p>Freenode服务器中有一个英语环境的IRC（*Internet Chat Relay）交流频道，叫#pepper&carrot。这个频道是专门供您用以讨论和小辣椒与萝卜头有关的内容的。我（昵称：deevad）经常去关注频道里的讨论话题。以下链接可以直接在您的浏览器里打开，而不需要安装任何插件。<br/> 
+',
+
+################################################################################
+# Page : Sources
+'SOURCES_TITLE'		    => '源文件',
+'SOURCES_TOP'	=> '
+    <p><b>欢迎来到源文件下载中心！</b><br/>
+    您在这里可以找到小辣椒与萝卜头的所有源文件。<br/>
+    数字绘图文件均可以用<a href="https://krita.org/">Krita</a>最新版本正确打开。</p>
+',
+'SOURCES_BOTTOM'	=> '
+    <p>下载这些文件的同时表明您默认同意<a href="http://creativecommons.org/licenses/by/4.0/deed.zh">Commons Attribution</a>发行许可的所有条款。<br/>
+    详情请参见每个项目中的README文件。<br/></p>
+  
+    <p><b>翻译，绘图，网站，脚本等源文件存放在：</b><br/></p>
+',
+'SOURCE_COVER'   =>  '封面',
+'SOURCE_KRITA'   =>  'Krita源文件',
+'SOURCE_TRANSLATOR'=>  '翻译者工具套装',
+'SOURCE_WEB'      =>  '互联网 <span class="sourceinfos"> 90ppi，轻量jpg文件</span>',
+'SOURCE_PRINT'        =>  '印刷 <span class="sourceinfos"> 300ppi，无损质量</span>',
+'SOURCE_MONTAGE'   =>  '整合 <span class="sourceinfos"> 所有画面聚集在一页图纸上</span>',
+
+################################################################################
+# Page : Author
+'AUTHOR_TITLE'		    => '关于David Revoy',
+'AUTHOR_BIO'	        => '
+    <p>大家好，我叫David Revoy，一名1981年出生在Reims现在居住在Montauban的法国绘画艺人。我的绘画是自学成才，并且我有很广泛的兴趣爱好：设计，绘画，猫咪，信息技术，开源技术，Gnu/Linux，互联网，老电子游戏，老日本漫画，艺术，日本文化和科幻……<br />
+    <br />
+    在以自由职业者的身份为出版商，教育部门，电子游戏工作室及动画电影产业工作了10年之后，我终于找到了适合我的，可以把我所有的兴趣爱好结合到一起的项目，那就是：小辣椒与萝卜头。</p>
+    
+    <p>我的主页：</p>
+',
+'AUTHOR_TODO_DREAM'	    => '
+    <h3>我对此项目的梦想：</h3>
+
+    <p>
+    ☐ 在日本的某个漫画大会上进行一次有关小辣椒与萝卜头的会议。<br/>
+    ☐ 用手柄玩一个以小辣椒与萝布头为创作原型的电子游戏。<br/>
+    ☐ 在本站画廊里拥有100个粉丝的艺术作品。<br/>
+    ☑ <a href="http://www.peppercarrot.com/en/article376/dream-to-do-list-a-wikipedia-page">拥有专属的维基页面。</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article387/cosplay-by-maria-and-ekaterina">收到一张小辣椒的cosplay照片。</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article304/dream-to-do-list-a-cat-named-carrot">收到一张以萝卜头命名的黄色背毛猫的照片。</a><br/>
+    ☑ <a href="http://www.peppercarrot.com/article302/peppercarrot-has-over-500-supporters">在Patreon网站上拥有500个赞助者。</a><br/>
+    ☐ 绘制100集漫画！<br/>
+    </p>
+',
+
+'AUTHOR_CARREER_TITLE'				    => '7个泡泡讲述我的经历：',
+'AUTHOR_CARREER_BUBBLE_DESCRIPTIONS'    => '<p>(图片内容为英语，无翻译)</p>',
+
+################################################################################
+# Page: Wiki
+'WIKI'		=> '维基',
+
+################################################################################
+# Website General : Footer
+'FOOTER_CONTENT'		=> '
+    <p>在没有特殊说明的情况下，本站的网络漫画，绘画作品以及文字均以<a href="https://creativecommons.org/licenses/by/4.0/deed.zh" title="更多信息请详见Creative Commons Attribution 4.0网站">Creative Commons Attribution 4.0</a>许可形式发行。<br/>
+    作者：David Revoy，www.davidrevoy.com。如需更多信息，请与我联系，<a href="mailto:info@davidrevoy.com">info@davidrevoy.com</a>。</p>
+    
+    <p>此网站使用<a href="http://www.pluxml.org" title="PlXml">PluXml</a></p> 
+',
+
+################################################################################
+# various utils :
+'UTIL_NEXT_EPISODE'     =>  '下一集',
+'UTIL_PREVIOUS_EPISODE' =>  '上一集',
+'UTIL_EPISODE'   =>  '漫画全集',
+'UTIL_BACK_TO_GALLERY'      =>  '返回到画廊',
+'UTIL_MORE'      =>  '更多',
+'UTIL_PAGE'      =>  '页面',
+'UTIL_BY'        =>  '由',
+'UTIL_LICENSE'   =>  '发行许可',
+'FIRST'        =>  '第一集',
+'LAST'   =>  '最新一集',
+
+);
+?>  
